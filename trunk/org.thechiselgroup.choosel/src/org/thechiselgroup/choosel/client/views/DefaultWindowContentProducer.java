3d9370d5161705b949b4e092ea7c53df820ba46f
Lars.Grammel <Lars.Grammel@2c9d0ac1-b0dc-320a-7a4d-fdcaf38e295f>
2010-04-25 03:46:15 +0000

    Removing dependency injector links from window content production
    - introduced WindowContentFactory
    - extracted ViewFactory
    - moved initialization into anonymous WindowContentFactories
    
    Separate framework code from application code 
    http://code.google.com/p/choosel/issues/detail?id=2

--- trunk/org.thechiselgroup.choosel/src/org/thechiselgroup/choosel/client/views/DefaultWindowContentProducer.java
+++ trunk/org.thechiselgroup.choosel/src/org/thechiselgroup/choosel/client/views/DefaultWindowContentProducer.java
@@ -34,12 +34,91 @@ import org.thechiselgroup.choosel.client.ui.NoteWindowContent;
 import org.thechiselgroup.choosel.client.ui.dnd.DropEnabledViewContentDisplay;
 import org.thechiselgroup.choosel.client.ui.dnd.ResourceSetAvatarDropTargetManager;
 import org.thechiselgroup.choosel.client.windows.WindowContent;
+import org.thechiselgroup.choosel.client.windows.WindowContentFactory;
 import org.thechiselgroup.choosel.client.windows.WindowContentProducer;
 
 import com.google.inject.Inject;
 import com.google.inject.name.Named;
 
 public class DefaultWindowContentProducer implements WindowContentProducer {
+
+    public static class ViewFactory implements WindowContentFactory {
+
+	private final ResourceSetAvatarFactory allResourcesDragAvatarFactory;
+
+	private ResourceCategorizer categorizer;
+
+	private ResourceSetAvatarDropTargetManager contentDropTargetManager;
+
+	private ResourceSet hoverModel;
+
+	private CategoryLabelProvider labelProvider;
+
+	private ResourceSetFactory resourceSetFactory;
+
+	private ResourceSetAvatarFactory selectionDragAvatarFactory;
+
+	private LabelProvider selectionModelLabelFactory;
+
+	private ResourceSetAvatarFactory typesDragAvatarFactory;
+
+	private ResourceSetAvatarFactory userSetsDragAvatarFactory;
+
+	private final String contentType;
+
+	private final ViewContentDisplayFactory viewContentDisplayFactory;
+
+	public ViewFactory(
+		String contentType,
+		ViewContentDisplayFactory viewContentDisplayFactory,
+		@Named(AVATAR_FACTORY_SET) ResourceSetAvatarFactory userSetsDragAvatarFactory,
+		@Named(AVATAR_FACTORY_TYPE) ResourceSetAvatarFactory typesDragAvatarFactory,
+		@Named(AVATAR_FACTORY_ALL_RESOURCES) ResourceSetAvatarFactory allResourcesDragAvatarFactory,
+		@Named(AVATAR_FACTORY_SELECTION) ResourceSetAvatarFactory selectionDragAvatarFactory,
+		@Named(HOVER_MODEL) ResourceSet hoverModel,
+		ResourceSetFactory resourceSetFactory,
+		@Named(LABEL_PROVIDER_SELECTION_SET) LabelProvider selectionModelLabelFactory,
+		ResourceCategorizer categorizer,
+		CategoryLabelProvider labelProvider,
+		@Named(DROP_TARGET_MANAGER_VIEW_CONTENT) ResourceSetAvatarDropTargetManager contentDropTargetManager) {
+
+	    this.contentType = contentType;
+	    this.viewContentDisplayFactory = viewContentDisplayFactory;
+
+	    this.userSetsDragAvatarFactory = userSetsDragAvatarFactory;
+	    this.typesDragAvatarFactory = typesDragAvatarFactory;
+	    this.allResourcesDragAvatarFactory = allResourcesDragAvatarFactory;
+	    this.selectionDragAvatarFactory = selectionDragAvatarFactory;
+	    this.contentDropTargetManager = contentDropTargetManager;
+	    this.hoverModel = hoverModel;
+	    this.resourceSetFactory = resourceSetFactory;
+	    this.selectionModelLabelFactory = selectionModelLabelFactory;
+	    this.categorizer = categorizer;
+	    this.labelProvider = labelProvider;
+	}
+
+	@Override
+	public WindowContent createWindowContent() {
+	    ResourceSplitter resourceSplitter = new ResourceSplitter(
+		    categorizer, resourceSetFactory, labelProvider);
+
+	    ViewContentDisplay contentDisplay = new DropEnabledViewContentDisplay(
+		    viewContentDisplayFactory.createViewContentDisplay(),
+		    contentDropTargetManager);
+
+	    return new DefaultView(hoverModel, selectionModelLabelFactory,
+		    resourceSetFactory,
+		    new ResourceSetAvatarResourceSetsPresenter(
+			    userSetsDragAvatarFactory),
+		    new ResourceSetAvatarResourceSetsPresenter(
+			    typesDragAvatarFactory),
+		    new ResourceSetAvatarResourceSetsPresenter(
+			    allResourcesDragAvatarFactory),
+		    new ResourceSetAvatarResourceSetsPresenter(
+			    selectionDragAvatarFactory), resourceSplitter,
+		    contentDisplay, contentType, contentType);
+	}
+    }
 
     private final ResourceSetAvatarFactory allResourcesDragAvatarFactory;
 
@@ -61,7 +140,7 @@ public class DefaultWindowContentProducer implements WindowContentProducer {
 
     private ResourceSetAvatarFactory userSetsDragAvatarFactory;
 
-    private Map<String, ViewContentDisplayFactory> viewContentDisplayFactories = new HashMap<String, ViewContentDisplayFactory>();
+    private Map<String, WindowContentFactory> viewContentDisplayFactories = new HashMap<String, WindowContentFactory>();
 
     @Inject
     public DefaultWindowContentProducer(
@@ -88,52 +167,55 @@ public class DefaultWindowContentProducer implements WindowContentProducer {
 	this.labelProvider = labelProvider;
     }
 
-    private ViewContentDisplay createContentDisplay(String contentType) {
+    private WindowContent createContentDisplay(String contentType) {
 	assert viewContentDisplayFactories.containsKey(contentType);
 	return viewContentDisplayFactories.get(contentType)
-		.createViewContentDisplay();
+		.createWindowContent();
     }
 
     public WindowContent createWindowContent(String contentType) {
 	assert contentType != null;
 
+	// TODO window content factories
+
 	// direct stuff
 	if ("ncbo-search".equals(contentType)) {
-	    return MashupClient.injector.createNCBOSearchViewContent();
+	    return new WindowContentFactory() {
+		@Override
+		public WindowContent createWindowContent() {
+		    return MashupClient.injector.createNCBOSearchViewContent();
+		}
+	    }.createWindowContent();
 	} else if ("help".equals(contentType)) {
-	    return new HelpWindowContent();
+	    return new WindowContentFactory() {
+		@Override
+		public WindowContent createWindowContent() {
+		    return new HelpWindowContent();
+		}
+	    }.createWindowContent();
 	} else if ("note".equals(contentType)) {
-	    return new NoteWindowContent();
+	    return new WindowContentFactory() {
+		@Override
+		public WindowContent createWindowContent() {
+		    return new NoteWindowContent();
+		}
+	    }.createWindowContent();
 	}
 
-	// otherwise: view
-	ResourceSplitter resourceSplitter = new ResourceSplitter(categorizer,
-		resourceSetFactory, labelProvider);
-
-	ViewContentDisplay contentDisplay = new DropEnabledViewContentDisplay(
-		createContentDisplay(contentType), contentDropTargetManager);
-
-	DefaultView view = new DefaultView(hoverModel,
-		selectionModelLabelFactory, resourceSetFactory,
-		new ResourceSetAvatarResourceSetsPresenter(
-			userSetsDragAvatarFactory),
-		new ResourceSetAvatarResourceSetsPresenter(
-			typesDragAvatarFactory),
-		new ResourceSetAvatarResourceSetsPresenter(
-			allResourcesDragAvatarFactory),
-		new ResourceSetAvatarResourceSetsPresenter(
-			selectionDragAvatarFactory), resourceSplitter,
-		contentDisplay, contentType, contentType);
-
-	return view;
+	return createContentDisplay(contentType);
     }
 
-    public void registerViewContentDisplayFactory(String contentType,
-	    ViewContentDisplayFactory viewContentDisplayFactory) {
+    public void registerViewContentDisplayFactory(final String contentType,
+	    final ViewContentDisplayFactory viewContentDisplayFactory) {
 
 	assert contentType != null;
 	assert viewContentDisplayFactory != null;
 
-	viewContentDisplayFactories.put(contentType, viewContentDisplayFactory);
+	viewContentDisplayFactories.put(contentType, new ViewFactory(
+		contentType, viewContentDisplayFactory,
+		userSetsDragAvatarFactory, typesDragAvatarFactory,
+		allResourcesDragAvatarFactory, selectionDragAvatarFactory,
+		hoverModel, resourceSetFactory, selectionModelLabelFactory,
+		categorizer, labelProvider, contentDropTargetManager));
     }
 }
