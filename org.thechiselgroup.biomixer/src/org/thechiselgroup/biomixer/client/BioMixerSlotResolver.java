3e894f9eaf6cf913a175fd95a9381e769b1f66bc
Lars.Grammel <Lars.Grammel@2c9d0ac1-b0dc-320a-7a4d-fdcaf38e295f>
2010-04-28 06:30:31 +0000

    Separated bio-mixer specific slot resolver behavior and moved it into Bio-Mixer
    Separate framework code from application code 
    http://code.google.com/p/choosel/issues/detail?id=2

--- org.thechiselgroup.biomixer/src/org/thechiselgroup/biomixer/client/BioMixerSlotResolver.java
+++ org.thechiselgroup.biomixer/src/org/thechiselgroup/biomixer/client/BioMixerSlotResolver.java
@@ -13,21 +13,25 @@
  * See the License for the specific language governing permissions and 
  * limitations under the License.  
  *******************************************************************************/
-package org.thechiselgroup.choosel.client.views;
+package org.thechiselgroup.biomixer.client;
 
 import java.util.List;
 
+import org.thechiselgroup.choosel.client.domain.ncbo.NCBO;
+import org.thechiselgroup.choosel.client.domain.ncbo.NcboUriHelper;
 import org.thechiselgroup.choosel.client.resolver.FixedValuePropertyValueResolver;
+import org.thechiselgroup.choosel.client.resolver.PropertyValueResolver;
 import org.thechiselgroup.choosel.client.resolver.PropertyValueResolverConverterWrapper;
 import org.thechiselgroup.choosel.client.resolver.SimplePropertyValueResolver;
-import org.thechiselgroup.choosel.client.test.ResourcesTestHelper;
+import org.thechiselgroup.choosel.client.resources.Resource;
 import org.thechiselgroup.choosel.client.util.ConversionException;
 import org.thechiselgroup.choosel.client.util.Converter;
+import org.thechiselgroup.choosel.client.views.Layer;
+import org.thechiselgroup.choosel.client.views.SlotResolver;
 
-public class DefaultSlotResolver implements SlotResolver {
+public class BioMixerSlotResolver implements SlotResolver {
 
     public void createColorSlotResolver(Layer layerModel, List<Layer> layers) {
-
 	String color = SlotResolver.COLORS[layers.size()];
 
 	layerModel.putResolver(COLOR_SLOT_ID,
@@ -45,23 +49,45 @@ public class DefaultSlotResolver implements SlotResolver {
 	// TODO resources as part of layerModel
 	// TODO how to do the automatic color assignment?
 	// TODO refactor // extract
-	if ("tsunami".equals(layerModel.getCategory())) {
+	if (NcboUriHelper.NCBO_CONCEPT.equals(layerModel.getCategory())) {
 	    layerModel.putResolver(SlotResolver.DESCRIPTION_SLOT_ID,
-		    new SimplePropertyValueResolver("date"));
-	} else if ("earthquake".equals(layerModel.getCategory())) {
+		    new PropertyValueResolver() {
+			@Override
+			public Object getValue(Resource resource) {
+			    return resource.getValue(NCBO.CONCEPT_NAME)
+				    + " [from: "
+				    + resource
+					    .getValue(NCBO.CONCEPT_ONTOLOGY_NAME)
+				    + "]";
+			}
+		    });
+	} else if (NcboUriHelper.NCBO_MAPPING.equals(layerModel.getCategory())) {
 	    layerModel.putResolver(SlotResolver.DESCRIPTION_SLOT_ID,
-		    new SimplePropertyValueResolver("description"));
-	} else if (ResourcesTestHelper.DEFAULT_TYPE.equals(layerModel
-		.getCategory())) {
-	    layerModel.putResolver(SlotResolver.DESCRIPTION_SLOT_ID,
-		    new SimplePropertyValueResolver(
-			    ResourcesTestHelper.LABEL_KEY));
+		    new PropertyValueResolver() {
+			@Override
+			public Object getValue(Resource resource) {
+			    return resource
+				    .getValue(NCBO.MAPPING_SOURCE_CONCEPT_NAME)
+				    + " ["
+				    + resource
+					    .getValue(NCBO.MAPPING_SOURCE_ONTOLOGY_NAME)
+				    + "] --> "
+				    + resource
+					    .getValue(NCBO.MAPPING_DESTINATION_CONCEPT_NAME)
+				    + " ["
+				    + resource
+					    .getValue(NCBO.MAPPING_DESTINATION_ONTOLOGY_NAME)
+				    + "]";
+			}
+		    });
 	} else {
 	    throw new RuntimeException("failed creating slot mapping");
 	}
     }
 
     public void createLabelSlotResolver(Layer layerModel) {
+	// TODO replace this code with null label slot resolver
+
 	Converter<Float, String> converter = new Converter<Float, String>() {
 	    @Override
 	    public String convert(Float value) throws ConversionException {
@@ -82,9 +108,6 @@ public class DefaultSlotResolver implements SlotResolver {
     }
 
     public void createLocationSlotResolver(Layer layer) {
-	layer.putResolver(SlotResolver.LOCATION_SLOT_ID,
-		new SimplePropertyValueResolver("location"));
-
     }
 
 }
