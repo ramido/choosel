73e4b6dad3be0b90f530bb2a0204f4a6ec0842ec
Lars.Grammel <Lars.Grammel@2c9d0ac1-b0dc-320a-7a4d-fdcaf38e295f>
2010-04-28 06:20:37 +0000

    Changed SlotResolver into interface, moved methods to DefaultSlotResolver implementation
    Separate framework code from application code 
    http://code.google.com/p/choosel/issues/detail?id=2

--- org.thechiselgroup.choosel/src/org/thechiselgroup/choosel/client/views/SlotResolver.java
+++ org.thechiselgroup.choosel/src/org/thechiselgroup/choosel/client/views/SlotResolver.java
@@ -17,121 +17,28 @@ package org.thechiselgroup.choosel.client.views;
 
 import java.util.List;
 
-import org.thechiselgroup.choosel.client.domain.ncbo.NCBO;
-import org.thechiselgroup.choosel.client.domain.ncbo.NcboUriHelper;
-import org.thechiselgroup.choosel.client.resolver.FixedValuePropertyValueResolver;
-import org.thechiselgroup.choosel.client.resolver.PropertyValueResolver;
-import org.thechiselgroup.choosel.client.resolver.PropertyValueResolverConverterWrapper;
-import org.thechiselgroup.choosel.client.resolver.SimplePropertyValueResolver;
-import org.thechiselgroup.choosel.client.resources.Resource;
-import org.thechiselgroup.choosel.client.test.ResourcesTestHelper;
-import org.thechiselgroup.choosel.client.util.ConversionException;
-import org.thechiselgroup.choosel.client.util.Converter;
+// TODO create more flexible slot system
+public interface SlotResolver {
 
-// TODO refactor to object
-public class SlotResolver {
+    String COLOR_SLOT_ID = "color";
 
-    public static final String COLOR_SLOT_ID = "color";
+    String[] COLORS = new String[] { "#6495ed", "#b22222" };
 
-    public static final String[] COLORS = new String[] { "#6495ed", "#b22222" };
+    String DATE_SLOT_ID = "date";
 
-    public static final String DATE_SLOT_ID = "date";
+    String DESCRIPTION_SLOT_ID = "description";
 
-    public static final String DESCRIPTION_SLOT_ID = "description";
+    String LABEL_SLOT_ID = "label";
 
-    public static final String LABEL_SLOT_ID = "label";
+    String LOCATION_SLOT_ID = "location";
 
-    public static final String LOCATION_SLOT_ID = "location";
+    void createLocationSlotResolver(Layer layer);
 
-    public void createColorSlotResolver(Layer layerModel, List<Layer> layers) {
+    void createLabelSlotResolver(Layer layerModel);
 
-	String color = SlotResolver.COLORS[layers.size()];
+    void createDescriptionSlotResolver(Layer layerModel);
 
-	layerModel.putResolver(COLOR_SLOT_ID,
-		new FixedValuePropertyValueResolver(color));
-    }
+    void createDateSlotResolver(Layer layerModel);
 
-    public void createDateSlotResolver(Layer layerModel) {
-	layerModel.putResolver(SlotResolver.DATE_SLOT_ID,
-		new SimplePropertyValueResolver("date"));
-
-    }
-
-    public void createDescriptionSlotResolver(Layer layerModel) {
-	// TODO switch based on category -- need category as part of layerModel
-	// TODO resources as part of layerModel
-	// TODO how to do the automatic color assignment?
-	// TODO refactor // extract
-	if ("tsunami".equals(layerModel.getCategory())) {
-	    layerModel.putResolver(SlotResolver.DESCRIPTION_SLOT_ID,
-		    new SimplePropertyValueResolver("date"));
-	} else if ("earthquake".equals(layerModel.getCategory())) {
-	    layerModel.putResolver(SlotResolver.DESCRIPTION_SLOT_ID,
-		    new SimplePropertyValueResolver("description"));
-	} else if (ResourcesTestHelper.DEFAULT_TYPE.equals(layerModel
-		.getCategory())) {
-	    layerModel.putResolver(SlotResolver.DESCRIPTION_SLOT_ID,
-		    new SimplePropertyValueResolver(
-			    ResourcesTestHelper.LABEL_KEY));
-	} else if (NcboUriHelper.NCBO_CONCEPT.equals(layerModel.getCategory())) {
-	    layerModel.putResolver(SlotResolver.DESCRIPTION_SLOT_ID,
-		    new PropertyValueResolver() {
-			@Override
-			public Object getValue(Resource resource) {
-			    return resource.getValue(NCBO.CONCEPT_NAME)
-				    + " [from: "
-				    + resource
-					    .getValue(NCBO.CONCEPT_ONTOLOGY_NAME)
-				    + "]";
-			}
-		    });
-	} else if (NcboUriHelper.NCBO_MAPPING.equals(layerModel.getCategory())) {
-	    layerModel.putResolver(SlotResolver.DESCRIPTION_SLOT_ID,
-		    new PropertyValueResolver() {
-			@Override
-			public Object getValue(Resource resource) {
-			    return resource
-				    .getValue(NCBO.MAPPING_SOURCE_CONCEPT_NAME)
-				    + " ["
-				    + resource
-					    .getValue(NCBO.MAPPING_SOURCE_ONTOLOGY_NAME)
-				    + "] --> "
-				    + resource
-					    .getValue(NCBO.MAPPING_DESTINATION_CONCEPT_NAME)
-				    + " ["
-				    + resource
-					    .getValue(NCBO.MAPPING_DESTINATION_ONTOLOGY_NAME)
-				    + "]";
-			}
-		    });
-	} else {
-	    throw new RuntimeException("failed creating slot mapping");
-	}
-    }
-
-    public void createLabelSlotResolver(Layer layerModel) {
-	Converter<Float, String> converter = new Converter<Float, String>() {
-	    @Override
-	    public String convert(Float value) throws ConversionException {
-		if (value != null) {
-		    int f = (value).intValue();
-		    return "" + f;
-		}
-
-		return "";
-	    }
-	};
-
-	SimplePropertyValueResolver resolver = new SimplePropertyValueResolver(
-		"magnitude");
-
-	layerModel.putResolver(SlotResolver.LABEL_SLOT_ID,
-		new PropertyValueResolverConverterWrapper(resolver, converter));
-    }
-
-    public void createLocationSlotResolver(Layer layer) {
-	layer.putResolver(SlotResolver.LOCATION_SLOT_ID,
-		new SimplePropertyValueResolver("location"));
-
-    }
+    void createColorSlotResolver(Layer layerModel, List<Layer> layers);
 }
