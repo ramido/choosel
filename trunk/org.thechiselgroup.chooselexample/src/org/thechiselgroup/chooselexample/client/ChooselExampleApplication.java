77813795943d118e839735a58832d7f0a77983e3
Lars.Grammel <Lars.Grammel@2c9d0ac1-b0dc-320a-7a4d-fdcaf38e295f>
2010-05-02 00:15:07 +0000

    Moving example code from Choosel into example project chooselexample
    Separate framework code from application code 
    http://code.google.com/p/choosel/issues/detail?id=2

---
+++ trunk/org.thechiselgroup.chooselexample/src/org/thechiselgroup/chooselexample/client/ChooselExampleApplication.java
@@ -0,0 +1,165 @@
+/*******************************************************************************
+ * Copyright 2009, 2010 Lars Grammel 
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License. 
+ * You may obtain a copy of the License at 
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0 
+ *     
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an "AS IS" BASIS, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+ * See the License for the specific language governing permissions and 
+ * limitations under the License.  
+ *******************************************************************************/
+package org.thechiselgroup.chooselexample.client;
+
+import java.util.Date;
+import java.util.Set;
+
+import org.thechiselgroup.choosel.client.ChooselApplication;
+import org.thechiselgroup.choosel.client.resources.Resource;
+import org.thechiselgroup.choosel.client.resources.ResourceSet;
+import org.thechiselgroup.choosel.client.resources.ResourceSetFactory;
+import org.thechiselgroup.choosel.client.resources.ui.ResourceSetAvatarResourceSetsPresenter;
+import org.thechiselgroup.choosel.client.resources.ui.ResourceSetsPresenter;
+import org.thechiselgroup.choosel.client.test.ResourcesTestHelper;
+import org.thechiselgroup.choosel.client.views.list.ListViewContentDisplay;
+import org.thechiselgroup.choosel.client.windows.AbstractWindowContent;
+import org.thechiselgroup.choosel.client.windows.CreateWindowCommand;
+import org.thechiselgroup.chooselexample.client.services.GeoRSSServiceAsync;
+
+import com.allen_sauer.gwt.log.client.Log;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.Widget;
+import com.google.inject.Inject;
+
+public class ChooselExampleApplication extends ChooselApplication {
+
+    public static final String DATA_PANEL = "data";
+
+    private static class DataSourceCallBack implements
+	    AsyncCallback<Set<Resource>> {
+
+	private ResourceSetsPresenter dataSourcesPresenter;
+
+	private String label;
+
+	private final ResourceSetFactory resourceSetFactory;
+
+	public DataSourceCallBack(String label,
+		ResourceSetsPresenter dataSourcesPresenter,
+		ResourceSetFactory resourceSetsFactory) {
+
+	    this.label = label;
+	    this.dataSourcesPresenter = dataSourcesPresenter;
+	    this.resourceSetFactory = resourceSetsFactory;
+	}
+
+	public void onFailure(Throwable e) {
+	    Log.error(e.getMessage(), e);
+	}
+
+	public void onSuccess(Set<Resource> resources) {
+	    ResourceSet resourceSet = resourceSetFactory.createResourceSet();
+	    resourceSet.addAll(resources);
+	    resourceSet.setLabel(label);
+
+	    dataSourcesPresenter.addResourceSet(resourceSet);
+	}
+    }
+
+    @Inject
+    private GeoRSSServiceAsync geoRssService;
+
+    private void addDataSourcesButton() {
+	Button b = new Button("Tsunami / Earthquake");
+	b.addClickHandler(new ClickHandler() {
+
+	    @Override
+	    public void onClick(ClickEvent event) {
+		final ResourceSetsPresenter dataSourcesPresenter = createResourceSetsPresenter();
+
+		// TODO this type cannot be stored yet
+		createWindow(new AbstractWindowContent("Data Sources", "TODO") {
+		    @Override
+		    public Widget asWidget() {
+			return dataSourcesPresenter.asWidget();
+		    }
+		});
+
+		geoRssService
+			.getGeoRSS(
+				"http://earthquake.usgs.gov/eqcenter/catalogs/shakerss.xml",
+				"earthquake", new DataSourceCallBack(
+					"earthquake", dataSourcesPresenter,
+					resourceSetsFactory));
+		geoRssService
+			.getGeoRSS(
+				"http://www.prh.noaa.gov/ptwc/feeds/ptwc_rss_pacific.xml",
+				"tsunami", new DataSourceCallBack("tsunami",
+					dataSourcesPresenter,
+					resourceSetsFactory));
+	    }
+
+	});
+	addWidget(DATA_PANEL, b);
+    }
+
+    // TODO change into command
+    private void addTestDataSourceButton() {
+	Button b = new Button("Test Data");
+	b.addClickHandler(new ClickHandler() {
+
+	    @Override
+	    public void onClick(ClickEvent event) {
+		String title = "TestResources";
+		final ResourceSetsPresenter dataSourcesPresenter = new ResourceSetAvatarResourceSetsPresenter(
+			defaultDragAvatarFactory);
+		dataSourcesPresenter.init();
+
+		commandManager.execute(new CreateWindowCommand(desktop,
+			new AbstractWindowContent(title, "TODO") {
+			    @Override
+			    public Widget asWidget() {
+				return dataSourcesPresenter.asWidget();
+			    }
+			}));
+
+		ResourceSet resourceSet = createResourceSet();
+		resourceSet.setLabel("Test");
+		resourceSet.addAll(ResourcesTestHelper.createResources(1, 2, 3,
+			4, 5));
+		for (Resource resource : resourceSet) {
+		    resource.putValue("date", new Date().toString());
+		}
+
+		dataSourcesPresenter.addResourceSet(resourceSet);
+	    }
+
+	});
+
+	addWidget(DATA_PANEL, b);
+    }
+
+    @Override
+    protected void initCustomActions() {
+	addDataSourcesButton();
+	addTestDataSourceButton();
+
+	addWindowContentButton(VIEWS_PANEL, "Note", "note");
+	addWindowContentButton(VIEWS_PANEL, "List", ListViewContentDisplay.TYPE);
+	addWindowContentButton(VIEWS_PANEL, "Map", "Map");
+	addWindowContentButton(VIEWS_PANEL, "Timeline", "Timeline");
+    }
+
+    @Override
+    protected void initCustomPanels() {
+	addPanel(VIEWS_PANEL, "Views");
+	addPanel(DATA_PANEL, "Data Sources");
+    }
+}
---
+++ trunk/org.thechiselgroup.chooselexample/src/org/thechiselgroup/chooselexample/server/GeoRSSServiceImpl.java
@@ -0,0 +1,178 @@
+/*******************************************************************************
+ * Copyright 2009, 2010 Lars Grammel 
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License. 
+ * You may obtain a copy of the License at 
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0 
+ *     
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an "AS IS" BASIS, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+ * See the License for the specific language governing permissions and 
+ * limitations under the License.  
+ *******************************************************************************/
+package org.thechiselgroup.chooselexample.server;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Iterator;
+import java.util.Set;
+import java.util.TimeZone;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.xml.XMLConstants;
+import javax.xml.namespace.NamespaceContext;
+import javax.xml.xpath.XPathExpressionException;
+
+import org.thechiselgroup.choosel.client.resources.Resource;
+import org.thechiselgroup.choosel.client.services.ServiceException;
+import org.thechiselgroup.choosel.server.XMLCallServlet;
+import org.thechiselgroup.chooselexample.client.services.GeoRSSService;
+import org.w3c.dom.Node;
+
+@SuppressWarnings("serial")
+public class GeoRSSServiceImpl extends XMLCallServlet implements GeoRSSService {
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+	super.init(config);
+
+	xpath.setNamespaceContext(new NamespaceContext() {
+
+	    public String getNamespaceURI(String prefix) {
+		if (prefix == null)
+		    throw new NullPointerException("Null prefix");
+
+		if ("georss".equals(prefix)) {
+		    return "http://www.georss.org/georss";
+		}
+		if ("geo".equals(prefix)) {
+		    return "http://www.w3.org/2003/01/geo/wgs84_pos#";
+		}
+		if ("eq".equals(prefix)) {
+		    return "http://earthquake.usgs.gov/rss/1.0/";
+		}
+		if ("xml".equals(prefix)) {
+		    return XMLConstants.XML_NS_URI;
+		}
+		return XMLConstants.NULL_NS_URI;
+	    }
+
+	    // This method isn't necessary for XPath processing.
+	    public String getPrefix(String uri) {
+		throw new UnsupportedOperationException();
+	    }
+
+	    // This method isn't necessary for XPath processing either.
+	    public Iterator getPrefixes(String uri) {
+		throw new UnsupportedOperationException();
+	    }
+
+	});
+
+	setupSetExpression("//rss/channel/item");
+
+	registerExpression("latitude", "geo:lat/text()");
+	registerExpression("longitude", "geo:long/text()");
+	registerExpression("title", "title/text()");
+	registerExpression("seconds", "eq:seconds/text()");
+	registerExpression("description", "description/text()");
+	registerExpression("guid", "guid/text()");
+	registerExpression("link", "link/text()");
+    }
+
+    @Override
+    protected Resource analyzeNode(Node node, String label) throws Exception {
+	Resource resource = null;
+
+	if ("earthquake".equalsIgnoreCase(label)) {
+	    String link = evaluateString("link", node);
+	    link = link.substring(0, link.length() - 2);
+	    resource = new Resource("earthquake:"
+		    + link.substring(link.lastIndexOf("/")));
+
+	    long milliseconds = evaluateNumber("seconds", node).longValue() * 1000;
+	    String titleString = evaluateString("title", node);
+	    int firstSplitIndex = titleString.indexOf('-');
+	    float magnitude = Float.parseFloat(titleString.substring(0,
+		    firstSplitIndex).trim());
+	    String locationDescription = titleString.substring(
+		    firstSplitIndex + 1).trim();
+
+	    resource.putValue("date", new Date(milliseconds).toString());
+	    resource.putValue("description", titleString);
+	    resource.putValue("magnitude", magnitude);
+	    resource.putValue("details", evaluateString("description", node));
+	    resource.putValue("locationDescription", locationDescription);
+	} else if ("tsunami".equalsIgnoreCase(label)) {
+	    resource = new Resource("tsunami:" + evaluateString("guid", node));
+
+	    String descriptionString = evaluateString("description", node);
+	    String[] split = descriptionString.split("\n");
+
+	    extractEarthquakeIssuedAt(resource, split);
+	    extractEarthquakeFromTsunami(resource, split);
+	    extractTsunamiEvaluation(resource, descriptionString);
+	}
+
+	resource.putValue("location", parseLocation(node));
+
+	return resource;
+    }
+
+    private void extractEarthquakeIssuedAt(Resource feedEntry, String[] split)
+	    throws ParseException {
+	String issuedAtLine = split[3];
+	String issuedAt = issuedAtLine.substring(10, issuedAtLine.length());
+	SimpleDateFormat format = new SimpleDateFormat("kkmm'Z' dd MMM yyyy");
+	format.setTimeZone(TimeZone.getTimeZone("UTC")); // assume UTC...
+	feedEntry.putValue("date", format.parse(issuedAt).toString());
+    }
+
+    private void extractTsunamiEvaluation(Resource r, String descriptionString) {
+	int i1 = descriptionString.indexOf("EVALUATION");
+	String d2 = descriptionString.substring(i1);
+	String[] s2 = d2.split("\n");
+	String evaluation = "";
+	for (int i = 1; i < s2.length; i++) {
+	    if (!(s2[i].length() == 0 || s2[i].charAt(0) == ' ')) {
+		break;
+	    }
+	    evaluation += s2[i].toLowerCase() + "<br>";
+	}
+
+	r.putValue("evaluation", evaluation);
+    }
+
+    private void extractEarthquakeFromTsunami(Resource r, String[] split) {
+	String earthquake = split[20] + "<br/>" + split[21] + "<br/>"
+		+ split[22] + "<br/>" + split[23] + "<br/>" + split[24];
+
+	r.putValue("related earthquake description", earthquake);
+    }
+
+    private Resource parseLocation(Node node) throws XPathExpressionException {
+
+	float latitude = evaluateNumber("latitude", node).floatValue();
+	float longitude = evaluateNumber("longitude", node).floatValue();
+
+	Resource location = new Resource("location:" + latitude + "/"
+		+ longitude);
+
+	location.putValue("latitude", latitude);
+	location.putValue("longitude", longitude);
+
+	return location;
+    }
+
+    @Override
+    public Set<Resource> getGeoRSS(String url, String label)
+	    throws ServiceException {
+	return analyzeXML(url, label);
+    }
+
+}
