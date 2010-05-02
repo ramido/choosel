1ccb00c76e21f502dbff5c100c0377abcf009e53
Lars.Grammel <Lars.Grammel@2c9d0ac1-b0dc-320a-7a4d-fdcaf38e295f>
2010-05-02 00:15:07 +0000

    Moving example code from Choosel into example project chooselexample
    Separate framework code from application code 
    http://code.google.com/p/choosel/issues/detail?id=2

---
+++ org.thechiselgroup.chooselexample/src/org/thechiselgroup/chooselexample/client/ChooselExampleSlotResolver.java
@@ -0,0 +1,79 @@
+/*******************************************************************************
+ * Copyright 2009, 2010 Lars Grammel 
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License. 
+ * You may obtain a copy of the License at 
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0 
+ *     
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an "AS IS" BASIS, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+ * See the License for the specific language governing permissions and 
+ * limitations under the License.  
+ *******************************************************************************/
+package org.thechiselgroup.chooselexample.client;
+
+import org.thechiselgroup.choosel.client.resolver.PropertyValueResolver;
+import org.thechiselgroup.choosel.client.resolver.PropertyValueResolverConverterWrapper;
+import org.thechiselgroup.choosel.client.resolver.SimplePropertyValueResolver;
+import org.thechiselgroup.choosel.client.test.ResourcesTestHelper;
+import org.thechiselgroup.choosel.client.util.ConversionException;
+import org.thechiselgroup.choosel.client.util.Converter;
+import org.thechiselgroup.choosel.client.views.DefaultSlotResolver;
+
+public class ChooselExampleSlotResolver extends DefaultSlotResolver {
+
+    @Override
+    public PropertyValueResolver createDescriptionSlotResolver(String category) {
+	// TODO switch based on category -- need category as part of layerModel
+	// TODO resources as part of layerModel
+	// TODO how to do the automatic color assignment?
+	// TODO refactor // extract
+	if ("tsunami".equals(category)) {
+	    return new SimplePropertyValueResolver("date");
+	}
+
+	if ("earthquake".equals(category)) {
+	    return new SimplePropertyValueResolver("description");
+	}
+
+	if (ResourcesTestHelper.DEFAULT_TYPE.equals(category)) {
+	    return new SimplePropertyValueResolver(
+		    ResourcesTestHelper.LABEL_KEY);
+	}
+
+	throw new RuntimeException("failed creating slot mapping");
+    }
+
+    @Override
+    public PropertyValueResolver createLabelSlotResolver(String category) {
+	Converter<Float, String> converter = new Converter<Float, String>() {
+	    @Override
+	    public String convert(Float value) throws ConversionException {
+		if (value != null) {
+		    int f = (value).intValue();
+		    return "" + f;
+		}
+
+		return "";
+	    }
+	};
+
+	SimplePropertyValueResolver resolver = new SimplePropertyValueResolver(
+		"magnitude");
+
+	return new PropertyValueResolverConverterWrapper(resolver, converter);
+    }
+
+    @Override
+    public PropertyValueResolver createDateSlotResolver(String type) {
+	return new SimplePropertyValueResolver("date");
+    }
+
+    @Override
+    public PropertyValueResolver createLocationSlotResolver(String category) {
+	return new SimplePropertyValueResolver("location");
+    }
+}
