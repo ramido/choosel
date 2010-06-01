d50186d82f41c7bdd1fe5178c39b5f775b866f11
bblashko <bblashko@2c9d0ac1-b0dc-320a-7a4d-fdcaf38e295f>
2010-06-01 20:31:02 +0000

    Added initial Protovis capability

---
+++ org.thechiselgroup.choosel/src/org/thechiselgroup/choosel/client/ui/widget/chart/LineChart.java
@@ -0,0 +1,26 @@
+package org.thechiselgroup.choosel.client.ui.widget.chart;
+
+import com.google.gwt.core.client.JavaScriptObject;
+
+public class LineChart extends ChartWidget {
+    
+    @Override
+    public native void drawGraph(JavaScriptObject chart, int width, int height) /*-{
+
+	var val = this.@org.thechiselgroup.choosel.client.ui.widget.chart.ChartWidget::val,
+	graph = this;
+        
+        chart.add($wnd.pv.Line)
+            .data(val)
+            .bottom(function(d) {return d * height / 8;})
+            .left(function() {return this.index * width / val.length + 15;})
+          .add($wnd.pv.Dot)
+            .event("click",function() {return graph.@org.thechiselgroup.choosel.client.ui.widget.chart.ChartWidget::onClick(III)(this.index,chart.mouse().x,chart.mouse().y);})
+            .event("mousemove",function() {return graph.@org.thechiselgroup.choosel.client.ui.widget.chart.ChartWidget::onMouseOver(III)(this.index,chart.mouse().x,chart.mouse().y);})
+            .event("mouseout",function() {return graph.@org.thechiselgroup.choosel.client.ui.widget.chart.ChartWidget::onMouseOut(III)(this.index,chart.mouse().x,chart.mouse().y);})
+          .root.render();
+
+    }-*/;
+    
+}
+
