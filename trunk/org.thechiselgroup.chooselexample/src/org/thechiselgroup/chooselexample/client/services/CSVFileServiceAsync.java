7f56aad150011da37359b65f81937981118ff558
patrickchisel <patrickchisel@2c9d0ac1-b0dc-320a-7a4d-fdcaf38e295f>
2010-06-25 23:33:28 +0000

    added a CSV service which works with avirishu's data added it to the slot resolver and the choosel example application

---
+++ trunk/org.thechiselgroup.chooselexample/src/org/thechiselgroup/chooselexample/client/services/CSVFileServiceAsync.java
@@ -0,0 +1,11 @@
+package org.thechiselgroup.chooselexample.client.services;
+
+import java.util.Set;
+
+import org.thechiselgroup.choosel.client.resources.Resource;
+
+import com.google.gwt.user.client.rpc.AsyncCallback;
+
+public interface CSVFileServiceAsync {
+    void getCSVResources(String filePath, String fileName, AsyncCallback<Set<Resource>> callback) throws Exception;
+}
