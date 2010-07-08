93222c7a041ce03a761f6746ea40a46b811457bf
Lars.Grammel <Lars.Grammel@2c9d0ac1-b0dc-320a-7a4d-fdcaf38e295f>
2010-07-08 05:48:45 +0000

    replace if-clause with assertion

--- trunk/org.thechiselgroup.choosel/src/org/thechiselgroup/choosel/client/resources/DefaultResourceManager.java
+++ trunk/org.thechiselgroup.choosel/src/org/thechiselgroup/choosel/client/resources/DefaultResourceManager.java
@@ -93,11 +93,8 @@ public class DefaultResourceManager implements ResourceManager {
 
     @Override
     public Resource getByUri(String uri) {
-        ResourceElement resourceElement = getResourceElement(uri);
-        //TODO make sure that this check does not break anything
-        if (resourceElement == null) {
-            return null;
-        }
+        assert keysToResourceElements.containsKey(uri) : "no resource for uri '"
+                + uri + "'";
         return getResourceElement(uri).resource;
     }
 
