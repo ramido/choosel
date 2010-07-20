6bc2f15bc98a2742600b0a847908c79364de0b93
Lars.Grammel <Lars.Grammel@2c9d0ac1-b0dc-320a-7a4d-fdcaf38e295f>
2010-07-20 18:11:07 +0000

    added SwitchingResourceSet

---
+++ trunk/org.thechiselgroup.choosel/jretest/org/thechiselgroup/choosel/client/resources/SwitchingResourceSetTest.java
@@ -0,0 +1,184 @@
+/*******************************************************************************
+ * Copyright 2009, 2010 Lars Grammel 
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License. 
+ * You may obtain a copy of the License at 
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0 
+ *     
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an "AS IS" BASIS, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+ * See the License for the specific language governing permissions and 
+ * limitations under the License.  
+ *******************************************************************************/
+package org.thechiselgroup.choosel.client.resources;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertSame;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.thechiselgroup.choosel.client.test.AdvancedAsserts.assertContentEquals;
+import static org.thechiselgroup.choosel.client.test.ResourcesTestHelper.captureOnResourcesAdded;
+import static org.thechiselgroup.choosel.client.test.ResourcesTestHelper.captureOnResourcesRemoved;
+import static org.thechiselgroup.choosel.client.test.TestResourceSetFactory.createResources;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+public class SwitchingResourceSetTest {
+
+    private SwitchingResourceSet underTest;
+
+    private ResourceSet[] resourceSets;
+
+    @Mock
+    private ResourcesAddedEventHandler resourcesAddedHandler;
+
+    @Mock
+    private ResourcesRemovedEventHandler resourcesRemovedHandler;
+
+    @Test
+    public void doNotFireResourcesAddedEventOnResourcesAddedToFormerDelegate() {
+        underTest.setDelegate(resourceSets[0]);
+        underTest.setDelegate(resourceSets[1]);
+        underTest.addHandler(ResourcesAddedEvent.TYPE, resourcesAddedHandler);
+
+        resourceSets[0].addAll(createResources(3, 4));
+
+        verify(resourcesAddedHandler, times(0)).onResourcesAdded(
+                any(ResourcesAddedEvent.class));
+    }
+
+    @Test
+    public void doNotFireResourcesRemovedEventOnResourcesRemovedFromFormerDelegate() {
+        underTest.setDelegate(resourceSets[0]);
+        underTest.addHandler(ResourcesAddedEvent.TYPE, resourcesAddedHandler);
+        underTest.setDelegate(resourceSets[1]);
+
+        resourceSets[0].removeAll(createResources(1, 2));
+
+        verify(resourcesRemovedHandler, times(0)).onResourcesRemoved(
+                any(ResourcesRemovedEvent.class));
+    }
+
+    @Test
+    public void fireResourcesAddedEventOnResourcesAddedToDelegate() {
+        underTest.setDelegate(resourceSets[0]);
+        underTest.addHandler(ResourcesAddedEvent.TYPE, resourcesAddedHandler);
+
+        resourceSets[0].addAll(createResources(3, 4));
+
+        ResourcesAddedEvent firedEvent = captureOnResourcesAdded(1,
+                resourcesAddedHandler).getValue();
+
+        assertContentEquals(createResources(3, 4).toList(),
+                firedEvent.getAddedResources());
+        assertSame(underTest, firedEvent.getTarget());
+    }
+
+    @Test
+    public void fireResourcesAddedEventOnResourcesAddedToNewDelegate() {
+        underTest.setDelegate(resourceSets[0]);
+        underTest.setDelegate(resourceSets[1]);
+        underTest.addHandler(ResourcesAddedEvent.TYPE, resourcesAddedHandler);
+
+        resourceSets[1].addAll(createResources(4, 5));
+
+        ResourcesAddedEvent firedEvent = captureOnResourcesAdded(1,
+                resourcesAddedHandler).getValue();
+
+        assertContentEquals(createResources(4, 5).toList(),
+                firedEvent.getAddedResources());
+        assertSame(underTest, firedEvent.getTarget());
+    }
+
+    @Test
+    public void fireResourcesEventOnDelegateChange() {
+        underTest.setDelegate(resourceSets[2]);
+        underTest.addHandler(ResourcesRemovedEvent.TYPE,
+                resourcesRemovedHandler);
+        underTest.addHandler(ResourcesAddedEvent.TYPE, resourcesAddedHandler);
+
+        underTest.setDelegate(resourceSets[3]);
+
+        ResourcesAddedEvent addedEvent = captureOnResourcesAdded(1,
+                resourcesAddedHandler).getValue();
+
+        assertContentEquals(createResources(4, 5).toList(),
+                addedEvent.getAddedResources());
+        assertSame(underTest, addedEvent.getTarget());
+
+        ResourcesRemovedEvent removedEvent = captureOnResourcesRemoved(1,
+                resourcesRemovedHandler).getValue();
+
+        assertContentEquals(createResources(1, 2).toList(),
+                removedEvent.getRemovedResources());
+        assertSame(underTest, removedEvent.getTarget());
+    }
+
+    @Test
+    public void fireResourcesRemovedEventOnResourcesRemovedFromDelegate() {
+        underTest.setDelegate(resourceSets[0]);
+        underTest.addHandler(ResourcesRemovedEvent.TYPE,
+                resourcesRemovedHandler);
+
+        resourceSets[0].removeAll(createResources(1, 2));
+
+        ResourcesRemovedEvent firedEvent = captureOnResourcesRemoved(1,
+                resourcesRemovedHandler).getValue();
+
+        assertContentEquals(createResources(1, 2).toList(),
+                firedEvent.getRemovedResources());
+        assertSame(underTest, firedEvent.getTarget());
+    }
+
+    @Test
+    public void fireResourcesRemovedEventOnResourcesRemovedFromNewDelegate() {
+        underTest.setDelegate(resourceSets[0]);
+        underTest.setDelegate(resourceSets[1]);
+        underTest.addHandler(ResourcesRemovedEvent.TYPE,
+                resourcesRemovedHandler);
+
+        resourceSets[1].removeAll(createResources(2, 3));
+
+        ResourcesRemovedEvent firedEvent = captureOnResourcesRemoved(1,
+                resourcesRemovedHandler).getValue();
+
+        assertContentEquals(createResources(2, 3).toList(),
+                firedEvent.getRemovedResources());
+        assertSame(underTest, firedEvent.getTarget());
+    }
+
+    @Test
+    public void getDelegateReturnsCurrentDelegate1() {
+        underTest.setDelegate(resourceSets[0]);
+
+        assertEquals(resourceSets[0], underTest.getDelegate());
+    }
+
+    @Test
+    public void getDelegateReturnsCurrentDelegate2() {
+        underTest.setDelegate(resourceSets[0]);
+        underTest.setDelegate(resourceSets[1]);
+
+        assertEquals(resourceSets[1], underTest.getDelegate());
+    }
+
+    @Before
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+
+        this.underTest = new SwitchingResourceSet();
+
+        this.resourceSets = new ResourceSet[4];
+        this.resourceSets[0] = createResources(1, 2);
+        this.resourceSets[1] = createResources(2, 3);
+        this.resourceSets[2] = createResources(1, 2, 3);
+        this.resourceSets[3] = createResources(3, 4, 5);
+    }
+}
---
+++ trunk/org.thechiselgroup.choosel/src/org/thechiselgroup/choosel/client/resources/SwitchingResourceSet.java
@@ -0,0 +1,145 @@
+/*******************************************************************************
+ * Copyright 2009, 2010 Lars Grammel 
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License. 
+ * You may obtain a copy of the License at 
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0 
+ *     
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an "AS IS" BASIS, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+ * See the License for the specific language governing permissions and 
+ * limitations under the License.  
+ *******************************************************************************/
+package org.thechiselgroup.choosel.client.resources;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.google.gwt.event.shared.GwtEvent.Type;
+import com.google.gwt.event.shared.HandlerManager;
+import com.google.gwt.event.shared.HandlerRegistration;
+
+/**
+ * <p>
+ * Resource set that delegates most operations to a delegate resource set. The
+ * event handlers, however, are managed by the switching resource set. The
+ * delegate of the resource set can be changed. If this happens, appropriate
+ * ResourcesAddedEvents and ResourcesRemovedEvents events are generated that
+ * reflect the changes in the contained resources because of the delegate set
+ * exchange.
+ * </p>
+ * <p>
+ * Labeling is ignored here and in fact just delegated.
+ * </p>
+ * 
+ * @author Lars Grammel
+ */
+// TODO separate labeling concept from resource set
+public class SwitchingResourceSet extends DelegatingResourceSet {
+
+    protected transient HandlerManager eventBus;
+
+    private ResourcesAddedEventHandler resourcesAddedToDelegateHandler = new ResourcesAddedEventHandler() {
+        @Override
+        public void onResourcesAdded(ResourcesAddedEvent e) {
+            forwardEvent(e);
+        }
+    };
+
+    private ResourcesRemovedEventHandler resourcesRemovedFromDelegateHandler = new ResourcesRemovedEventHandler() {
+        @Override
+        public void onResourcesRemoved(ResourcesRemovedEvent e) {
+            forwardEvent(e);
+        }
+    };
+
+    private HandlerRegistration resourcesAddedToDelegateHandlerRegistration;
+
+    private HandlerRegistration resourcesRemovedFromDelegateHandlerRegistration;
+
+    public SwitchingResourceSet() {
+        super(null);
+        this.eventBus = new HandlerManager(this);
+    }
+
+    private void addEventHandlersToDelegate() {
+        resourcesAddedToDelegateHandlerRegistration = delegate.addHandler(
+                ResourcesAddedEvent.TYPE, resourcesAddedToDelegateHandler);
+        resourcesRemovedFromDelegateHandlerRegistration = delegate
+                .addHandler(ResourcesRemovedEvent.TYPE,
+                        resourcesRemovedFromDelegateHandler);
+    }
+
+    @Override
+    public <H extends ResourceEventHandler> HandlerRegistration addHandler(
+            Type<H> type, H handler) {
+        return eventBus.addHandler(type, handler);
+    }
+
+    private void fireResourceChanges(ResourceSet newDelegate,
+            ResourceSet oldDelegate) {
+
+        if (oldDelegate != null) {
+            List<Resource> removedResources = new ArrayList<Resource>();
+            removedResources.addAll(oldDelegate.toList());
+            if (newDelegate != null) {
+                removedResources.removeAll(newDelegate.toList());
+            }
+            fireResourcesRemoved(removedResources);
+        }
+
+        if (newDelegate != null) {
+            List<Resource> addedResources = new ArrayList<Resource>();
+            addedResources.addAll(newDelegate.toList());
+            if (oldDelegate != null) {
+                addedResources.removeAll(oldDelegate.toList());
+            }
+            fireResourcesAdded(addedResources);
+        }
+    }
+
+    private void fireResourcesAdded(List<Resource> addedResources) {
+        eventBus.fireEvent(new ResourcesAddedEvent(this, addedResources));
+    }
+
+    private void fireResourcesRemoved(List<Resource> removedResources) {
+        eventBus.fireEvent(new ResourcesRemovedEvent(this, removedResources));
+    }
+
+    protected void forwardEvent(ResourcesAddedEvent e) {
+        fireResourcesAdded(e.getAddedResources());
+    }
+
+    protected void forwardEvent(ResourcesRemovedEvent e) {
+        fireResourcesRemoved(e.getRemovedResources());
+    }
+
+    public boolean hasDelegate() {
+        return this.delegate != null;
+    }
+
+    private void removeEventHandlersFromDelegate() {
+        if (hasDelegate()) {
+            resourcesAddedToDelegateHandlerRegistration.removeHandler();
+            resourcesRemovedFromDelegateHandlerRegistration.removeHandler();
+        }
+
+        resourcesAddedToDelegateHandlerRegistration = null;
+        resourcesRemovedFromDelegateHandlerRegistration = null;
+    }
+
+    public void setDelegate(ResourceSet newDelegate) {
+        removeEventHandlersFromDelegate();
+
+        // TODO calculate event & fire
+        ResourceSet oldDelegate = this.delegate;
+        this.delegate = newDelegate;
+
+        fireResourceChanges(newDelegate, oldDelegate);
+
+        addEventHandlersToDelegate();
+    }
+}
