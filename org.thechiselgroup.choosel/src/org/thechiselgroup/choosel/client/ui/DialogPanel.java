d91326acae26a450c89c7b185e0e5d8c0d51d875
Lars.Grammel <Lars.Grammel@2c9d0ac1-b0dc-320a-7a4d-fdcaf38e295f>
2010-09-19 22:32:36 +0000

    Fixed DialogPanel size calculations
    ACCEPTED - task 75: CSV import 
    http://code.google.com/p/choosel/issues/detail?id=75

--- org.thechiselgroup.choosel/src/org/thechiselgroup/choosel/client/ui/DialogPanel.java
+++ org.thechiselgroup.choosel/src/org/thechiselgroup/choosel/client/ui/DialogPanel.java
@@ -27,6 +27,8 @@ import com.google.gwt.user.client.ui.Widget;
 
 public class DialogPanel extends Panel {
 
+    private static final int BUTTON_BAR_CSS_MARGIN = 4;
+
     public static final String CSS_DIALOG_PANEL = "choosel-DialogPanel";
 
     public static final String CSS_DIALOG_PANEL_BUTTONBAR = "choosel-DialogPanel-ButtonBar";
@@ -221,7 +223,11 @@ public class DialogPanel extends Panel {
         super.setPixelSize(width, height);
 
         int contentHeight = getOffsetHeight() - headerLabel.getOffsetHeight()
-                - buttonBar.getOffsetHeight();
+                - buttonBar.getOffsetHeight() - BUTTON_BAR_CSS_MARGIN;
+
+        if (contentHeight < 0) {
+            contentHeight = 0;
+        }
 
         contentWidget.setHeight(contentHeight + CSS.PX);
     }
