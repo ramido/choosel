2fd490011dd96b639ba422af9adf12ed37235c64
Lars.Grammel <Lars.Grammel@2c9d0ac1-b0dc-320a-7a4d-fdcaf38e295f>
2010-09-29 04:10:38 +0000

    preparing WindowResizeController for testing
    ACCEPTED - task 76: resizing windows below required content size possible 
    http://code.google.com/p/choosel/issues/detail?id=76

--- org.thechiselgroup.choosel/src/org/thechiselgroup/choosel/client/windows/WindowPanel.java
+++ org.thechiselgroup.choosel/src/org/thechiselgroup/choosel/client/windows/WindowPanel.java
@@ -53,24 +53,7 @@ import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.Widget;
 
 public class WindowPanel extends NEffectPanel implements
-        DragProxyEventReceiver, Opacity {
-
-    /**
-     * WindowPanel direction constant, used in
-     * {@link WindowResizeController#makeDraggable(com.google.gwt.user.client.ui.Widget, org.thechiselgroup.mashups.client.windows.demo.client.example.resize.WindowPanel.DirectionConstant)}
-     * .
-     */
-    public static class DirectionConstant {
-
-        public final int directionBits;
-
-        public final String directionLetters;
-
-        private DirectionConstant(int directionBits, String directionLetters) {
-            this.directionBits = directionBits;
-            this.directionLetters = directionLetters;
-        }
-    }
+        DragProxyEventReceiver, Opacity, ResizeablePanel {
 
     private static final int BORDER_THICKNESS = 7;
 
@@ -86,85 +69,11 @@ public class WindowPanel extends NEffectPanel implements
 
     private static final String CSS_WINDOW_RESIZE_EDGE = "choosel-WindowPanel-ResizeEdge";
 
-    /**
-     * Specifies that resizing occur at the east edge.
-     */
-    public static final int DIRECTION_EAST = 0x0001;
-
-    /**
-     * Specifies that resizing occur at the both edge.
-     */
-    public static final int DIRECTION_NORTH = 0x0002;
-
-    /**
-     * Specifies that resizing occur at the south edge.
-     */
-    public static final int DIRECTION_SOUTH = 0x0004;
-
-    /**
-     * Specifies that resizing occur at the west edge.
-     */
-    public static final int DIRECTION_WEST = 0x0008;
-
-    /**
-     * Specifies that resizing occur at the east edge.
-     */
-    public static final DirectionConstant EAST = new DirectionConstant(
-            DIRECTION_EAST, "e");
-
-    public static final DirectionConstant EAST_TOP = new DirectionConstant(
-            DIRECTION_EAST, "et");
-
     private static final String IMAGE_CLOSE_ACTIVE = "images/close_active.gif";
 
     private static final String IMAGE_CLOSE_INVISIBLE = "images/close_invisible.gif";
 
     private static final String IMAGE_CLOSE_VISIBLE = "images/close_visible.gif";
-
-    /**
-     * Specifies that resizing occur at the both edge.
-     */
-    public static final DirectionConstant NORTH = new DirectionConstant(
-            DIRECTION_NORTH, "n");
-
-    /**
-     * Specifies that resizing occur at the north-east edge.
-     */
-    public static final DirectionConstant NORTH_EAST = new DirectionConstant(
-            DIRECTION_NORTH | DIRECTION_EAST, "ne");
-
-    /**
-     * Specifies that resizing occur at the north-west edge.
-     */
-    public static final DirectionConstant NORTH_WEST = new DirectionConstant(
-            DIRECTION_NORTH | DIRECTION_WEST, "nw");
-
-    /**
-     * Specifies that resizing occur at the south edge.
-     */
-    public static final DirectionConstant SOUTH = new DirectionConstant(
-            DIRECTION_SOUTH, "s");
-
-    /**
-     * Specifies that resizing occur at the south-east edge.
-     */
-    public static final DirectionConstant SOUTH_EAST = new DirectionConstant(
-            DIRECTION_SOUTH | DIRECTION_EAST, "se");
-
-    /**
-     * Specifies that resizing occur at the south-west edge.
-     */
-    public static final DirectionConstant SOUTH_WEST = new DirectionConstant(
-            DIRECTION_SOUTH | DIRECTION_WEST, "sw");
-
-    /**
-     * Specifies that resizing occur at the west edge.
-     */
-    public static final DirectionConstant WEST = new DirectionConstant(
-            DIRECTION_WEST, "w");
-
-    public static final DirectionConstant WEST_TOP = new DirectionConstant(
-            DIRECTION_WEST, "wt");
 
     private static final int WINDOW_BORDER_FROM_CSS = 2 + 2; // outer + inner
 
@@ -411,6 +320,7 @@ public class WindowPanel extends NEffectPanel implements
         return "Close";
     }
 
+    @Override
     public int getHeight() {
         return height;
     }
@@ -419,6 +329,7 @@ public class WindowPanel extends NEffectPanel implements
         return getModuleBase() + IMAGE_CLOSE_INVISIBLE;
     }
 
+    @Override
     public int getMinimumWidth() {
         int minimumWidth = 20;
 
@@ -439,6 +350,12 @@ public class WindowPanel extends NEffectPanel implements
         return getModuleBase() + IMAGE_CLOSE_VISIBLE;
     }
 
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.thechiselgroup.choosel.client.windows.ResizeablePanel#getWidth()
+     */
+    @Override
     public int getWidth() {
         return width;
     }
@@ -534,6 +451,7 @@ public class WindowPanel extends NEffectPanel implements
         addEffect(showEffect);
     }
 
+    @Override
     public void moveBy(int right, int down) {
         AbsolutePanel parent = (AbsolutePanel) getParent();
         Location location = new WidgetLocation(this, parent);
