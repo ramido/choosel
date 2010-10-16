fadc34e9f1fffdd05cef46aea1e75dddeddfce41
Lars.Grammel <Lars.Grammel@2c9d0ac1-b0dc-320a-7a4d-fdcaf38e295f>
2010-10-16 18:00:50 +0000

    removed tsunami/earthquake data

--- trunk/org.thechiselgroup.chooselexample/src/org/thechiselgroup/chooselexample/client/ChooselExampleApplication.java
+++ trunk/org.thechiselgroup.chooselexample/src/org/thechiselgroup/chooselexample/client/ChooselExampleApplication.java
@@ -16,101 +16,26 @@
 package org.thechiselgroup.chooselexample.client;
 
 import java.util.Date;
-import java.util.Set;
 
 import org.thechiselgroup.choosel.client.ChooselApplication;
 import org.thechiselgroup.choosel.client.configuration.ChooselInjectionConstants;
 import org.thechiselgroup.choosel.client.resources.Resource;
 import org.thechiselgroup.choosel.client.resources.ResourceSet;
-import org.thechiselgroup.choosel.client.resources.ResourceSetFactory;
 import org.thechiselgroup.choosel.client.resources.ui.ResourceSetAvatarResourceSetsPresenter;
 import org.thechiselgroup.choosel.client.resources.ui.ResourceSetsPresenter;
 import org.thechiselgroup.choosel.client.test.TestResourceSetFactory;
 import org.thechiselgroup.choosel.client.windows.AbstractWindowContent;
 import org.thechiselgroup.choosel.client.windows.CreateWindowCommand;
-import org.thechiselgroup.chooselexample.client.services.GeoRSSServiceAsync;
 
-import com.allen_sauer.gwt.log.client.Log;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.user.client.Random;
-import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.Widget;
-import com.google.inject.Inject;
 
 public class ChooselExampleApplication extends ChooselApplication {
 
-	private static class DataSourceCallBack implements
-			AsyncCallback<Set<Resource>> {
-
-		private ResourceSetsPresenter dataSourcesPresenter;
-
-		private String label;
-
-		private final ResourceSetFactory resourceSetFactory;
-
-		public DataSourceCallBack(String label,
-				ResourceSetsPresenter dataSourcesPresenter,
-				ResourceSetFactory resourceSetsFactory) {
-
-			this.label = label;
-			this.dataSourcesPresenter = dataSourcesPresenter;
-			this.resourceSetFactory = resourceSetsFactory;
-		}
-
-		public void onFailure(Throwable e) {
-			Log.error(e.getMessage(), e);
-		}
-
-		public void onSuccess(Set<Resource> resources) {
-			ResourceSet resourceSet = resourceSetFactory.createResourceSet();
-			resourceSet.addAll(resources);
-			resourceSet.setLabel(label);
-
-			dataSourcesPresenter.addResourceSet(resourceSet);
-		}
-	}
-
 	public static final String DATA_PANEL = "data";
-
-	@Inject
-	private GeoRSSServiceAsync geoRssService;
-
-	private void addDataSourcesButton() {
-		Button geoRssButton = new Button("Ts/Eq");
-		geoRssButton.addClickHandler(new ClickHandler() {
-
-			@Override
-			public void onClick(ClickEvent event) {
-				final ResourceSetsPresenter dataSourcesPresenter = createResourceSetsPresenter();
-
-				// TODO this type cannot be stored yet
-				createWindow(new AbstractWindowContent("Data Sources", "TODO") {
-					@Override
-					public Widget asWidget() {
-						return dataSourcesPresenter.asWidget();
-					}
-				});
-
-				geoRssService
-						.getGeoRSS(
-								"http://earthquake.usgs.gov/eqcenter/catalogs/shakerss.xml",
-								"earthquake", new DataSourceCallBack(
-										"earthquake", dataSourcesPresenter,
-										resourceSetsFactory));
-				geoRssService
-						.getGeoRSS(
-								"http://www.prh.noaa.gov/ptwc/feeds/ptwc_rss_pacific.xml",
-								"tsunami", new DataSourceCallBack("tsunami",
-										dataSourcesPresenter,
-										resourceSetsFactory));
-			}
-
-		});
-
-		addWidget(DEVELOPER_MODE_PANEL, geoRssButton);
-	}
 
 	// TODO change into command
 	private void addTestDataSourceButton() {
@@ -171,7 +96,6 @@ public class ChooselExampleApplication extends ChooselApplication {
 	@Override
 	protected void initCustomActions() {
 		if (runsInDevelopmentMode()) {
-			addDataSourcesButton();
 			addTestDataSourceButton();
 		}
 
