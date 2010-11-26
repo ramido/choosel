b5d98d0687de6eff4f5fa936a5e609d647371a6f
Lars.Grammel <Lars.Grammel@2c9d0ac1-b0dc-320a-7a4d-fdcaf38e295f>
2010-11-26 19:25:57 +0000

    moved visual mapping initialization into DefaultView
    ACCEPTED - task 103: Sharing of single views 
    http://code.google.com/p/choosel/issues/detail?id=103

--- org.thechiselgroup.choosel/src/org/thechiselgroup/choosel/client/views/VisualMappingsControl.java
+++ org.thechiselgroup.choosel/src/org/thechiselgroup/choosel/client/views/VisualMappingsControl.java
@@ -16,35 +16,23 @@
 package org.thechiselgroup.choosel.client.views;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
-import org.thechiselgroup.choosel.client.calculation.SumCalculation;
-import org.thechiselgroup.choosel.client.resolver.FixedValuePropertyValueResolver;
-import org.thechiselgroup.choosel.client.resolver.ResourceSetToValueResolver;
 import org.thechiselgroup.choosel.client.resources.ResourceByPropertyMultiCategorizer;
 import org.thechiselgroup.choosel.client.resources.ResourceSet;
 import org.thechiselgroup.choosel.client.resources.ResourceSetUtils;
 import org.thechiselgroup.choosel.client.resources.ResourceSplitter;
 import org.thechiselgroup.choosel.client.ui.ConfigurationPanel;
 import org.thechiselgroup.choosel.client.ui.WidgetAdaptable;
+import org.thechiselgroup.choosel.client.util.NullConverter;
+import org.thechiselgroup.choosel.client.views.widget.listbox.ExtendedListBox;
+import org.thechiselgroup.choosel.client.views.widget.listbox.ListBoxControl;
 
 import com.google.gwt.event.dom.client.ChangeEvent;
 import com.google.gwt.event.dom.client.ChangeHandler;
-import com.google.gwt.event.shared.HandlerRegistration;
-import com.google.gwt.user.client.ui.ListBox;
 import com.google.gwt.user.client.ui.Widget;
 
 public class VisualMappingsControl implements WidgetAdaptable {
-
-    /*
-     * Boolean flag that indicates if the configuration part of the view has
-     * been created.
-     * 
-     * XXX This solution breaks down when there is more than one kind of
-     * resource (i.e. with different properties)
-     */
-    private boolean isConfigurationAvailable = false;
 
     private final ResourceSplitter splitter;
 
@@ -54,15 +42,11 @@ public class VisualMappingsControl implements WidgetAdaptable {
 
     private final ViewContentDisplay contentDisplay;
 
-    private ListBox groupingBox;
+    private ListBoxControl<String> groupingBox;
 
     private DataTypeToListMap<SlotControl> slotControlsByDataType;
 
     private Map<Slot, SlotControl> slotToSlotControls = new HashMap<Slot, SlotControl>();
-
-    private HandlerRegistration groupingChangeHandlerRegistration;
-
-    private ChangeHandler groupingChangeHandler;
 
     public VisualMappingsControl(ViewContentDisplay contentDisplay,
             ResourceItemValueResolver resolver, ResourceSplitter splitter) {
@@ -100,23 +84,20 @@ public class VisualMappingsControl implements WidgetAdaptable {
         // TODO include aggregation that does not aggregate...
         // TODO include bin aggregation for numerical slots
 
-        groupingBox = new ListBox(false);
-        groupingBox.setVisibleItemCount(1);
+        groupingBox = new ListBoxControl<String>(new ExtendedListBox(false),
+                new NullConverter<String>());
 
-        groupingChangeHandler = new ChangeHandler() {
+        groupingBox.setChangeHandler(new ChangeHandler() {
             @Override
             public void onChange(ChangeEvent event) {
-                String property = groupingBox.getValue(groupingBox
-                        .getSelectedIndex());
-
+                String property = groupingBox.getSelectedValue();
                 splitter.setCategorizer(new ResourceByPropertyMultiCategorizer(
                         property));
             }
-        };
-        groupingChangeHandlerRegistration = groupingBox
-                .addChangeHandler(groupingChangeHandler);
+        });
 
-        visualMappingPanel.addConfigurationSetting("Grouping", groupingBox);
+        visualMappingPanel.addConfigurationSetting("Grouping",
+                groupingBox.asWidget());
     }
 
     private void initSlotControls() {
@@ -136,71 +117,10 @@ public class VisualMappingsControl implements WidgetAdaptable {
         }
     }
 
-    private void setInitialMappings(
-            DataTypeToListMap<String> propertiesByDataType) {
-
-        for (Slot slot : contentDisplay.getSlots()) {
-            DataType dataType = slot.getDataType();
-            List<String> properties = propertiesByDataType.get(dataType);
-
-            /*
-             * XXX this is actually a problem for the properties besides color.
-             * If we don't have a property of that type, the data cannot be
-             * visualized.
-             */
-            if (properties.isEmpty() && dataType != DataType.COLOR) {
-                continue;
-            }
-
-            ResourceSetToValueResolver setToValueResolver = null;
-
-            switch (dataType) {
-            case TEXT:
-                setToValueResolver = new TextResourceSetToValueResolver(
-                        properties.get(0));
-                break;
-            case NUMBER:
-                setToValueResolver = new CalculationResourceSetToValueResolver(
-                        properties.get(0), new SumCalculation());
-                break;
-            case DATE:
-                setToValueResolver = new FirstResourcePropertyResolver(
-                        properties.get(0));
-                break;
-            case COLOR:
-                setToValueResolver = new FixedValuePropertyValueResolver(
-                        "#6495ed");
-                break;
-            case LOCATION:
-                setToValueResolver = new FirstResourcePropertyResolver(
-                        properties.get(0));
-                break;
-            }
-
-            resolver.put(slot, setToValueResolver);
-        }
-    }
-
     // TODO link to resource model instead & do updates when resources change
     public void updateConfiguration(ResourceSet resources) {
         DataTypeToListMap<String> propertiesByDataType = ResourceSetUtils
                 .getPropertiesByDataType(resources);
-
-        /*
-         * TODO check if there are changes when adding / adjust each slot -->
-         * stable per slot --> initialize early for the slots & map to object
-         * that has corresponding update method
-         * 
-         * XXX for now: just add a flag if a configuration has been created, and
-         * if that's the case, don't rebuild the configuration.
-         * 
-         * XXX this also fails with redo / undo
-         */
-        // TODO check the validity of the configuration instead
-        if (!isConfigurationAvailable) {
-            setInitialMappings(propertiesByDataType);
-            isConfigurationAvailable = true;
-        }
 
         // TODO update selection of slots?
         updateGroupingBox(propertiesByDataType);
@@ -210,17 +130,10 @@ public class VisualMappingsControl implements WidgetAdaptable {
     private void updateGroupingBox(
             DataTypeToListMap<String> propertiesByDataType) {
 
-        groupingChangeHandlerRegistration.removeHandler();
-        groupingBox.clear();
-        for (String property : propertiesByDataType.get(DataType.TEXT)) {
-            groupingBox.addItem(property, property);
-        }
-        // TODO select correct index
-        groupingChangeHandlerRegistration = groupingBox
-                .addChangeHandler(groupingChangeHandler);
+        groupingBox.setValues(propertiesByDataType.get(DataType.TEXT));
     }
 
-    protected void updateSlotControls(
+    private void updateSlotControls(
             DataTypeToListMap<String> propertiesByDataType) {
 
         for (DataType dataType : DataType.values()) {
