a10c36939ad67302de0c9799db407650f173bb77
Lars.Grammel <Lars.Grammel@2c9d0ac1-b0dc-320a-7a4d-fdcaf38e295f>
2010-11-29 20:01:19 +0000

    saving / loading count calculations
    ACCEPTED - task 103: Sharing of single views 
    http://code.google.com/p/choosel/issues/detail?id=103

--- trunk/org.thechiselgroup.choosel/jretest/org/thechiselgroup/choosel/client/views/DefaultViewPersistenceTest.java
+++ trunk/org.thechiselgroup.choosel/jretest/org/thechiselgroup/choosel/client/views/DefaultViewPersistenceTest.java
@@ -23,6 +23,7 @@ import java.util.List;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.MockitoAnnotations;
+import org.thechiselgroup.choosel.client.calculation.CountCalculation;
 import org.thechiselgroup.choosel.client.persistence.Memento;
 import org.thechiselgroup.choosel.client.resources.Resource;
 import org.thechiselgroup.choosel.client.resources.ResourceByPropertyMultiCategorizer;
@@ -36,7 +37,9 @@ public class DefaultViewPersistenceTest {
 
     private TestView restoredView;
 
-    private Slot slot;
+    private Slot textSlot;
+
+    private Slot numberSlot;
 
     @Test
     public void restoreChangedTextSlot() {
@@ -45,10 +48,10 @@ public class DefaultViewPersistenceTest {
         resource.putValue("property1", "value1");
         resource.putValue("property2", "value2");
 
-        originalView.getSlotMappingConfiguration().setMapping(slot,
+        originalView.getSlotMappingConfiguration().setMapping(textSlot,
                 new FirstResourcePropertyResolver("property1"));
         originalView.getResourceModel().addResources(toResourceSet(resource));
-        originalView.getSlotMappingConfiguration().setMapping(slot,
+        originalView.getSlotMappingConfiguration().setMapping(textSlot,
                 new FirstResourcePropertyResolver("property2"));
 
         // 2. save first view
@@ -62,7 +65,46 @@ public class DefaultViewPersistenceTest {
         List<ResourceItem> resourceItems = restoredView.getResourceItems();
         assertEquals(1, resourceItems.size());
         ResourceItem resourceItem = resourceItems.get(0);
-        assertEquals("value2", resourceItem.getResourceValue(slot));
+        assertEquals("value2", resourceItem.getResourceValue(textSlot));
+    }
+
+    @Test
+    public void restoreCountCalculationOverGroup() {
+        // 1. create view and configure it - resources, settings...
+        Resource r1 = new Resource("test:1");
+        r1.putValue("property1", new Double(0));
+        r1.putValue("property2", "value2");
+
+        Resource r2 = new Resource("test:2");
+        r2.putValue("property1", new Double(3));
+        r2.putValue("property2", "value2");
+
+        Resource r3 = new Resource("test:3");
+        r3.putValue("property1", new Double(5));
+        r3.putValue("property2", "value2");
+
+        originalView.getResourceModel().addResources(toResourceSet(r1, r2, r3));
+        originalView.getResourceGrouping().setCategorizer(
+                new ResourceByPropertyMultiCategorizer("property2"));
+        originalView.getSlotMappingConfiguration().setMapping(
+                numberSlot,
+                new CalculationResourceSetToValueResolver("property1",
+                        new CountCalculation()));
+
+        // 2. save first view
+        DefaultResourceSetCollector collector = new DefaultResourceSetCollector();
+        Memento memento = originalView.save(collector);
+
+        // 3. restore other view - set by uri categorization first
+        restoredView.getResourceGrouping().setCategorizer(
+                new ResourceByUriMultiCategorizer());
+        restoredView.restore(memento, collector);
+
+        // 4. check resource items and control settings
+        List<ResourceItem> resourceItems = restoredView.getResourceItems();
+        assertEquals(1, resourceItems.size());
+        ResourceItem resourceItem = resourceItems.get(0);
+        assertEquals(3d, resourceItem.getResourceValue(numberSlot));
     }
 
     @Test
@@ -132,10 +174,11 @@ public class DefaultViewPersistenceTest {
     public void setUp() {
         MockitoAnnotations.initMocks(this);
 
-        slot = new Slot("id-1", "text-slot", DataType.TEXT);
+        textSlot = new Slot("id-1", "text-slot", DataType.TEXT);
+        numberSlot = new Slot("id-2", "number-slot", DataType.NUMBER);
 
-        originalView = TestView.createTestView(slot);
-        restoredView = TestView.createTestView(slot);
+        originalView = TestView.createTestView(textSlot, numberSlot);
+        restoredView = TestView.createTestView(textSlot, numberSlot);
     }
 
 }
--- trunk/org.thechiselgroup.choosel/src/org/thechiselgroup/choosel/client/views/CalculationResourceSetToValueResolver.java
+++ trunk/org.thechiselgroup.choosel/src/org/thechiselgroup/choosel/client/views/CalculationResourceSetToValueResolver.java
@@ -37,6 +37,10 @@ public class CalculationResourceSetToValueResolver implements
         this.calculation = calculation;
     }
 
+    public Calculation getCalculation() {
+        return calculation;
+    }
+
     public String getProperty() {
         return property;
     }
