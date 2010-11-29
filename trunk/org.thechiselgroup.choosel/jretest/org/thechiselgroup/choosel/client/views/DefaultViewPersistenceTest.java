ce568486e56d898ce4f7e40293581ab898ceb666
Lars.Grammel <Lars.Grammel@2c9d0ac1-b0dc-320a-7a4d-fdcaf38e295f>
2010-11-29 20:11:48 +0000

    saving / loading calculations in slot mappings
    ACCEPTED - task 103: Sharing of single views 
    http://code.google.com/p/choosel/issues/detail?id=103

--- trunk/org.thechiselgroup.choosel/jretest/org/thechiselgroup/choosel/client/views/DefaultViewPersistenceTest.java
+++ trunk/org.thechiselgroup.choosel/jretest/org/thechiselgroup/choosel/client/views/DefaultViewPersistenceTest.java
@@ -23,7 +23,12 @@ import java.util.List;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.MockitoAnnotations;
+import org.thechiselgroup.choosel.client.calculation.AverageCalculation;
+import org.thechiselgroup.choosel.client.calculation.Calculation;
 import org.thechiselgroup.choosel.client.calculation.CountCalculation;
+import org.thechiselgroup.choosel.client.calculation.MaxCalculation;
+import org.thechiselgroup.choosel.client.calculation.MinCalculation;
+import org.thechiselgroup.choosel.client.calculation.SumCalculation;
 import org.thechiselgroup.choosel.client.persistence.Memento;
 import org.thechiselgroup.choosel.client.resources.Resource;
 import org.thechiselgroup.choosel.client.resources.ResourceByPropertyMultiCategorizer;
@@ -40,6 +45,11 @@ public class DefaultViewPersistenceTest {
     private Slot textSlot;
 
     private Slot numberSlot;
+
+    @Test
+    public void restoreAverageCalculationOverGroup() {
+        testRestoreCalculationOverGroup(4d, new AverageCalculation());
+    }
 
     @Test
     public void restoreChangedTextSlot() {
@@ -70,41 +80,17 @@ public class DefaultViewPersistenceTest {
 
     @Test
     public void restoreCountCalculationOverGroup() {
-        // 1. create view and configure it - resources, settings...
-        Resource r1 = new Resource("test:1");
-        r1.putValue("property1", new Double(0));
-        r1.putValue("property2", "value2");
+        testRestoreCalculationOverGroup(3d, new CountCalculation());
+    }
 
-        Resource r2 = new Resource("test:2");
-        r2.putValue("property1", new Double(3));
-        r2.putValue("property2", "value2");
+    @Test
+    public void restoreMaxCalculationOverGroup() {
+        testRestoreCalculationOverGroup(8d, new MaxCalculation());
+    }
 
-        Resource r3 = new Resource("test:3");
-        r3.putValue("property1", new Double(5));
-        r3.putValue("property2", "value2");
-
-        originalView.getResourceModel().addResources(toResourceSet(r1, r2, r3));
-        originalView.getResourceGrouping().setCategorizer(
-                new ResourceByPropertyMultiCategorizer("property2"));
-        originalView.getSlotMappingConfiguration().setMapping(
-                numberSlot,
-                new CalculationResourceSetToValueResolver("property1",
-                        new CountCalculation()));
-
-        // 2. save first view
-        DefaultResourceSetCollector collector = new DefaultResourceSetCollector();
-        Memento memento = originalView.save(collector);
-
-        // 3. restore other view - set by uri categorization first
-        restoredView.getResourceGrouping().setCategorizer(
-                new ResourceByUriMultiCategorizer());
-        restoredView.restore(memento, collector);
-
-        // 4. check resource items and control settings
-        List<ResourceItem> resourceItems = restoredView.getResourceItems();
-        assertEquals(1, resourceItems.size());
-        ResourceItem resourceItem = resourceItems.get(0);
-        assertEquals(3d, resourceItem.getResourceValue(numberSlot));
+    @Test
+    public void restoreMinCalculationOverGroup() {
+        testRestoreCalculationOverGroup(0d, new MinCalculation());
     }
 
     @Test
@@ -139,6 +125,11 @@ public class DefaultViewPersistenceTest {
         assertEquals(2, resourceItemResources.size());
         assertEquals(true, resourceItemResources.contains(r1));
         assertEquals(true, resourceItemResources.contains(r2));
+    }
+
+    @Test
+    public void restoreSumCalculationOverGroup() {
+        testRestoreCalculationOverGroup(12d, new SumCalculation());
     }
 
     @Test
@@ -179,6 +170,46 @@ public class DefaultViewPersistenceTest {
 
         originalView = TestView.createTestView(textSlot, numberSlot);
         restoredView = TestView.createTestView(textSlot, numberSlot);
+    }
+
+    protected void testRestoreCalculationOverGroup(double expectedResult,
+            Calculation calculation) {
+
+        // 1. create view and configure it - resources, settings...
+        Resource r1 = new Resource("test:1");
+        r1.putValue("property1", new Double(0));
+        r1.putValue("property2", "value2");
+
+        Resource r2 = new Resource("test:2");
+        r2.putValue("property1", new Double(4));
+        r2.putValue("property2", "value2");
+
+        Resource r3 = new Resource("test:3");
+        r3.putValue("property1", new Double(8));
+        r3.putValue("property2", "value2");
+
+        originalView.getResourceModel().addResources(toResourceSet(r1, r2, r3));
+        originalView.getResourceGrouping().setCategorizer(
+                new ResourceByPropertyMultiCategorizer("property2"));
+        originalView.getSlotMappingConfiguration().setMapping(
+                numberSlot,
+                new CalculationResourceSetToValueResolver("property1",
+                        calculation));
+
+        // 2. save first view
+        DefaultResourceSetCollector collector = new DefaultResourceSetCollector();
+        Memento memento = originalView.save(collector);
+
+        // 3. restore other view - set by uri categorization first
+        restoredView.getResourceGrouping().setCategorizer(
+                new ResourceByUriMultiCategorizer());
+        restoredView.restore(memento, collector);
+
+        // 4. check resource items and control settings
+        List<ResourceItem> resourceItems = restoredView.getResourceItems();
+        assertEquals(1, resourceItems.size());
+        ResourceItem resourceItem = resourceItems.get(0);
+        assertEquals(expectedResult, resourceItem.getResourceValue(numberSlot));
     }
 
 }
