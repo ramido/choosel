409091029959d6e209a2b69972bc67e00a9a1d90
Lars.Grammel <Lars.Grammel@2c9d0ac1-b0dc-320a-7a4d-fdcaf38e295f>
2010-11-30 22:00:15 +0000

    documentation

--- trunk/org.thechiselgroup.choosel/src/org/thechiselgroup/choosel/client/authentication/AuthenticationManager.java
+++ trunk/org.thechiselgroup.choosel/src/org/thechiselgroup/choosel/client/authentication/AuthenticationManager.java
@@ -17,17 +17,43 @@ package org.thechiselgroup.choosel.client.authentication;
 
 import com.google.gwt.event.shared.HandlerRegistration;
 
+/**
+ * Provides access to the authentication state. This class can be used to check
+ * if the user has been authenticated, to check what the exact authentication
+ * state is, and to register handlers that get notified when the authentication
+ * state changes (e.g. the user logs on or off).
+ * 
+ * @author Lars Grammel
+ */
 public interface AuthenticationManager {
 
     HandlerRegistration addAuthenticationStateChangedEventHandler(
             AuthenticationStateChangedEventHandler handler);
 
+    /**
+     * Returns the current authentication information, which contains
+     * information about the user as well as the login and logout urls.
+     */
     Authentication getAuthentication();
 
+    /**
+     * Returns the authentication state. If you just need to know if a user is
+     * authenticated or not, you should use {@link #isAuthenticated()}.
+     * 
+     * @return current authentication state
+     */
     AuthenticationState getAuthenticationState();
 
+    /**
+     * @return exception that caused the authentication to fail, if the
+     *         authentication state is <code>FAILED</code>, and
+     *         <code>null</code> otherwise.
+     */
     Throwable getFailure();
 
+    /**
+     * @return true if the user is signed in
+     */
     boolean isAuthenticated();
 
 }
