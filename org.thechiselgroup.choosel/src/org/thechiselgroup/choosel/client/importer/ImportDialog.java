f951e5f8844efc81081a1d748838bca89d9bc6c1
Lars.Grammel <Lars.Grammel@2c9d0ac1-b0dc-320a-7a4d-fdcaf38e295f>
2010-12-06 06:05:47 +0000

    re-enabled and increased CSV import limitations
    ACCEPTED - task 105: Increase scalability 
    http://code.google.com/p/choosel/issues/detail?id=105

--- org.thechiselgroup.choosel/src/org/thechiselgroup/choosel/client/importer/ImportDialog.java
+++ org.thechiselgroup.choosel/src/org/thechiselgroup/choosel/client/importer/ImportDialog.java
@@ -147,22 +147,22 @@ public class ImportDialog extends AbstractDialog {
     public void okay() throws ParseException {
         String pastedText = pasteArea.getText();
 
-        // TODO re-enable
-        // if (pastedText.length() > 50000) {
-        // throw new ParseException(
-        // "The pasted text is too big. This demo supports only up to 50000 characters in the pasted text.");
-        // }
-        //
+        if (pastedText.length() > 75000) {
+            throw new ParseException(
+                    "The pasted text is too big. "
+                            + "This demo supports only up to 75000 characters in the pasted text.");
+        }
+
         StringTable parsedRows = new CSVStringTableParser().parse(pastedText);
-        //
-        // if (parsedRows.getColumnCount() > 15) {
-        // throw new ParseException(
-        // "Too many columns. This demo supports only up to 15 columns.");
-        // }
-        // if (parsedRows.getRowCount() > 200) {
-        // throw new ParseException(
-        // "Too many rows. This demo supports only up to 200 rows.");
-        // }
+
+        if (parsedRows.getColumnCount() > 20) {
+            throw new ParseException(
+                    "Too many columns. This demo supports only up to 20 columns.");
+        }
+        if (parsedRows.getRowCount() > 400) {
+            throw new ParseException(
+                    "Too many rows. This demo supports only up to 400 rows.");
+        }
 
         ResourceSet parsedResources = importer.createResources(parsedRows);
         parsedResources.setLabel(nameTextBox.getText());
