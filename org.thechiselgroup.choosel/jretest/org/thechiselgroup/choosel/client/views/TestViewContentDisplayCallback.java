b6618aa5511860a92eec0e39db148a2dfcc24cab
Lars.Grammel <Lars.Grammel@2c9d0ac1-b0dc-320a-7a4d-fdcaf38e295f>
2010-12-12 01:19:41 +0000

    extracted test view content display refactored GraphViewContentDisplayTest added test for showing previously created arc items later when all required resource items are added to the graph
    ACCEPTED - task 124: Separate logic for displaying arc in graph from logic for expanding graph 
    http://code.google.com/p/choosel/issues/detail?id=124

---
+++ org.thechiselgroup.choosel/jretest/org/thechiselgroup/choosel/client/views/TestViewContentDisplayCallback.java
@@ -0,0 +1,74 @@
+/*******************************************************************************
+ * Copyright 2009, 2010 Lars Grammel 
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License. 
+ * You may obtain a copy of the License at 
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0 
+ *     
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an "AS IS" BASIS, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+ * See the License for the specific language governing permissions and 
+ * limitations under the License.  
+ *******************************************************************************/
+package org.thechiselgroup.choosel.client.views;
+
+import java.util.Map;
+
+import org.thechiselgroup.choosel.client.resources.ResourceSet;
+import org.thechiselgroup.choosel.client.util.collections.CollectionFactory;
+import org.thechiselgroup.choosel.client.util.collections.LightweightCollection;
+import org.thechiselgroup.choosel.client.util.collections.LightweightList;
+
+// TODO extract resource item manager?
+public class TestViewContentDisplayCallback implements
+        ViewContentDisplayCallback {
+
+    private Map<String, ResourceItem> resourceItemsByGroupId = CollectionFactory
+            .createStringMap();
+
+    public void addResourceItem(ResourceItem resourceItem) {
+        resourceItemsByGroupId.put(resourceItem.getGroupID(), resourceItem);
+    }
+
+    public void addResourceItems(Iterable<ResourceItem> resourceItems) {
+        for (ResourceItem resourceItem : resourceItems) {
+            addResourceItem(resourceItem);
+        }
+    }
+
+    @Override
+    public boolean containsResourceItem(String groupId) {
+        return resourceItemsByGroupId.containsKey(groupId);
+    }
+
+    @Override
+    public ResourceSet getAutomaticResourceSet() {
+        return null;
+    }
+
+    @Override
+    public ResourceItem getResourceItemByGroupID(String groupId) {
+        return resourceItemsByGroupId.get(groupId);
+    }
+
+    @Override
+    public LightweightCollection<ResourceItem> getResourceItems() {
+        LightweightList<ResourceItem> result = CollectionFactory
+                .createLightweightList();
+        result.addAll(resourceItemsByGroupId.values());
+        return result;
+    }
+
+    @Override
+    public String getSlotResolverDescription(Slot slot) {
+        return null;
+    }
+
+    @Override
+    public void switchSelection(ResourceSet resources) {
+    }
+
+}
