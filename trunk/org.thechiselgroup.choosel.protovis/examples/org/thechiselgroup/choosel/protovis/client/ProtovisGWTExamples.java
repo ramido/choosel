7344a576c957fd9f3b2e1f6a9398d94c59bae543
Lars.Grammel <Lars.Grammel@2c9d0ac1-b0dc-320a-7a4d-fdcaf38e295f>
2010-12-29 14:08:10 +0000

    added Gas and Driving example
    ACCEPTED - task 131: Extract Protovis library wrapper as separate GWT module 
    http://code.google.com/p/choosel/issues/detail?id=131

--- trunk/org.thechiselgroup.choosel.protovis/examples/org/thechiselgroup/choosel/protovis/client/ProtovisGWTExamples.java
+++ trunk/org.thechiselgroup.choosel.protovis/examples/org/thechiselgroup/choosel/protovis/client/ProtovisGWTExamples.java
@@ -52,6 +52,7 @@ public class ProtovisGWTExamples implements EntryPoint {
 
     private void initExampleVisualizations() {
         examples = new ArrayList<ProtovisExample>();
+        examples.add(new GasAndDrivingExample());
         examples.add(new AreaChartExample());
         examples.add(new BarChartExample());
         examples.add(new PieChartExample());
