eca6f0b766329c50df0c461cc649d2894d2cabb3
lars.grammel@gmail.com <lars.grammel@gmail.com@2c9d0ac1-b0dc-320a-7a4d-fdcaf38e295f>
2011-01-22 23:45:57 +0000

    STARTED - task 133: Modularization http://code.google.com/p/choosel/issues/detail?id=133

---
+++ trunk/org.thechiselgroup.choosel.visualization_component.text/src/org/thechiselgroup/choosel/visualization_component/text/client/TextItem.java
@@ -0,0 +1,165 @@
+/*******************************************************************************
+ * Copyright 2009, 2010 Lars Grammel 
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License. 
+ * You may obtain a copy of the License at 
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0 
+ *     
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an "AS IS" BASIS, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+ * See the License for the specific language governing permissions and 
+ * limitations under the License.  
+ *******************************************************************************/
+package org.thechiselgroup.choosel.visualization_component.text.client;
+
+import org.thechiselgroup.choosel.core.client.ui.popup.DefaultPopupManager;
+import org.thechiselgroup.choosel.core.client.views.ViewItem;
+
+public class TextItem {
+
+    public static final String CSS_HIGHLIGHTED = "textItemHighlighted";
+
+    public static final String CSS_PARTIALLY_HIGHLIGHTED = "textItemPartiallyHighlighted";
+
+    public static final String CSS_LIST = "textItem";
+
+    public static final String CSS_SELECTED = "textItemSelected";
+
+    private TextItemLabel label;
+
+    private ViewItem resourceItem;
+
+    /**
+     * Flag that marks if the label of this text item has already been added to
+     * the container panel. Used to increase the performance of adding multiple
+     * text items to the view.
+     */
+    private boolean addedToPanel = false;
+
+    private String lastFontSize;
+
+    private String cachedDescription;
+
+    public TextItem(ViewItem resourceItem) {
+        assert resourceItem != null;
+
+        this.resourceItem = resourceItem;
+    }
+
+    public String getDescriptionValue() {
+        return (String) resourceItem
+                .getResourceValue(TextViewContentDisplay.DESCRIPTION_SLOT);
+    }
+
+    public double getFontSizeValue() {
+        return ((Double) resourceItem
+                .getResourceValue(TextViewContentDisplay.FONT_SIZE_SLOT))
+                .doubleValue();
+    }
+
+    public TextItemLabel getLabel() {
+        return label;
+    }
+
+    public ViewItem getResourceItem() {
+        return resourceItem;
+    }
+
+    public void init(TextItemLabel label) {
+        this.label = label;
+
+        label.addStyleName(CSS_LIST);
+
+        DefaultPopupManager.linkManagerToSource(resourceItem.getPopupManager(),
+                getLabel());
+
+        updateContent();
+    }
+
+    public boolean isAddedToPanel() {
+        return addedToPanel;
+    }
+
+    /**
+     * <p>
+     * <b>IMPLEMENTATION NOTE</b>: the last calculated font size gets cached and
+     * is compared to the result of the current font size calculation to prevent
+     * expensive DOM styling operations.
+     * </p>
+     */
+    public void scaleFont(DoubleToGroupValueMapper<String> groupValueMapper) {
+        String newFontSizeLabelValue = groupValueMapper
+                .getGroupValue(getFontSizeValue());
+
+        setFontSize(newFontSizeLabelValue);
+    }
+
+    public void setAddedToPanel(boolean addedToPanel) {
+        this.addedToPanel = addedToPanel;
+    }
+
+    public void setFontSize(String newFontSize) {
+        if (lastFontSize == null || newFontSize.compareTo(lastFontSize) != 0) {
+            label.setFontSize(newFontSize);
+            lastFontSize = newFontSize;
+        }
+    }
+
+    public void updateContent() {
+        // TODO what is this for
+        if (label == null) {
+            return;
+        }
+
+        /*
+         * PERFORMANCE: cache description and font size and only update UI
+         * elements when there is a change. This makes a huge difference with
+         * several thousand text items.
+         */
+        String description = getDescriptionValue();
+
+        if (cachedDescription == null || !cachedDescription.equals(description)) {
+            label.setText(description);
+            cachedDescription = description;
+        }
+    }
+
+    public void updateStatusStyling() {
+        switch (resourceItem.getHighlightStatus()) {
+        case COMPLETE: {
+            label.addStyleName(CSS_HIGHLIGHTED);
+            label.removeStyleName(CSS_PARTIALLY_HIGHLIGHTED);
+        }
+            break;
+        case PARTIAL: {
+            label.removeStyleName(CSS_HIGHLIGHTED);
+            label.addStyleName(CSS_PARTIALLY_HIGHLIGHTED);
+        }
+            break;
+        case NONE: {
+            label.removeStyleName(CSS_HIGHLIGHTED);
+            label.removeStyleName(CSS_PARTIALLY_HIGHLIGHTED);
+        }
+            break;
+        }
+
+        switch (resourceItem.getSelectionStatus()) {
+        case COMPLETE: {
+            label.addStyleName(CSS_SELECTED);
+        }
+            break;
+        case PARTIAL: {
+            label.addStyleName(CSS_SELECTED);
+        }
+            break;
+        case NONE: {
+            label.removeStyleName(CSS_SELECTED);
+        }
+            break;
+        }
+    }
+
+}
---
+++ trunk/org.thechiselgroup.choosel.visualization_component.text/src/org/thechiselgroup/choosel/visualization_component/text/client/TextViewContentDisplayFactory.java
@@ -0,0 +1,38 @@
+/*******************************************************************************
+ * Copyright 2009, 2010 Lars Grammel 
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License. 
+ * You may obtain a copy of the License at 
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0 
+ *     
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an "AS IS" BASIS, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+ * See the License for the specific language governing permissions and 
+ * limitations under the License.  
+ *******************************************************************************/
+package org.thechiselgroup.choosel.visualization_component.text.client;
+
+import org.thechiselgroup.choosel.core.client.ui.dnd.ResourceSetAvatarDragController;
+import org.thechiselgroup.choosel.core.client.views.ViewContentDisplay;
+import org.thechiselgroup.choosel.core.client.views.ViewContentDisplayFactory;
+
+import com.google.inject.Inject;
+
+public class TextViewContentDisplayFactory implements ViewContentDisplayFactory {
+
+    @Inject
+    private ResourceSetAvatarDragController dragController;
+
+    @Inject
+    public TextViewContentDisplayFactory() {
+    }
+
+    @Override
+    public ViewContentDisplay createViewContentDisplay() {
+        return new TextViewContentDisplay(dragController);
+    }
+
+}
