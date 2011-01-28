42ba0ec069bad223f05bc76c73a4d4a44e6f4d54
Lars.Grammel@gmail.com <Lars.Grammel@gmail.com@2c9d0ac1-b0dc-320a-7a4d-fdcaf38e295f>
2011-01-28 22:40:45 +0000

    STARTED - task 133: Modularization http://code.google.com/p/choosel/issues/detail?id=133

--- trunk/org.thechiselgroup.choosel.visualization_component.graph/src/org/thechiselgroup/choosel/visualization_component/graph/client/GraphVisualization.java
+++ trunk/org.thechiselgroup.choosel.visualization_component.graph/src/org/thechiselgroup/choosel/visualization_component/graph/client/GraphVisualization.java
@@ -1,6 +1,16 @@
 package org.thechiselgroup.choosel.visualization_component.graph.client;
 
+import org.thechiselgroup.choosel.core.client.resources.DataType;
+import org.thechiselgroup.choosel.core.client.views.slots.Slot;
+
 public final class GraphVisualization {
+
+    public static final Slot NODE_BORDER_COLOR_SLOT = new Slot(
+    "nodeBorderColor", "Node Border Color", DataType.COLOR);
+    public static final Slot NODE_BACKGROUND_COLOR_SLOT = new Slot(
+    "nodeBackgroundColor", "Node Color", DataType.COLOR);
+    public static final Slot NODE_LABEL_SLOT = new Slot("nodeLabel",
+    "Node Label", DataType.TEXT);
 
     private GraphVisualization() {
     }
