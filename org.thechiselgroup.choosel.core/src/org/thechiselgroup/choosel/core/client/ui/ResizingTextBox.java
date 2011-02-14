f50541d8c477a63b8f153266188a1bea676ba746
Lars.Grammel <Lars.Grammel@2c9d0ac1-b0dc-320a-7a4d-fdcaf38e295f>
2011-02-14 23:26:44 +0000

    Refactoring: Extracted TextBoundsEstimator
    ACCEPTED - task 64: Long label texts in Bar chart will cause overlap 
    http://code.google.com/p/choosel/issues/detail?id=64

--- org.thechiselgroup.choosel.core/src/org/thechiselgroup/choosel/core/client/ui/ResizingTextBox.java
+++ org.thechiselgroup.choosel.core/src/org/thechiselgroup/choosel/core/client/ui/ResizingTextBox.java
@@ -23,14 +23,10 @@ import com.google.gwt.event.dom.client.KeyPressEvent;
 import com.google.gwt.event.dom.client.KeyPressHandler;
 import com.google.gwt.event.dom.client.KeyUpEvent;
 import com.google.gwt.event.dom.client.KeyUpHandler;
-import com.google.gwt.user.client.DOM;
 import com.google.gwt.user.client.Element;
-import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.TextBox;
 
 public class ResizingTextBox extends TextBox {
-
-    private static final String CSS_TEXT_WIDTH_ESTIMATOR = "choosel-TextWidthEstimator";
 
     /*
      * The extra width should roughly accommodate another character, because the
@@ -38,30 +34,6 @@ public class ResizingTextBox extends TextBox {
      * the text to move.
      */
     private static final int EXTRA_WIDTH = 15;
-
-    // TODO move to CSS class
-    /**
-     * Returns the actual (computed) style for an element. This is necessary,
-     * because the regular style accessor returns an empty String if the style
-     * was not explicitly set on the corresponding element.
-     */
-    // @formatter:off
-    private static native String getStyle(Element el, String styleProp)/*-{
-        var camelize = function (str) {
-          return str.replace(/\-(\w)/g, function(str, letter){
-            return letter.toUpperCase();
-          });
-        };
-
-        if (el.currentStyle) {
-          return el.currentStyle[camelize(styleProp)];
-        } else if ($wnd.document.defaultView && $wnd.document.defaultView.getComputedStyle) {
-          return $wnd.document.defaultView.getComputedStyle(el,null).getPropertyValue(styleProp);
-        } else {
-          return el.style[camelize(styleProp)]; 
-        }
-    }-*/;
-    // @formatter:on
 
     private final int minWidth;
 
@@ -121,34 +93,7 @@ public class ResizingTextBox extends TextBox {
     }
 
     private void updateWidth(String text) {
-        // create invisible div with no padding, no margin, no border
-        Element estimatorElement = DOM.createSpan();
-        estimatorElement.setClassName(CSS_TEXT_WIDTH_ESTIMATOR);
-
-        // set font-family and font-size
-        DOM.setStyleAttribute(estimatorElement, "fontFamily",
-                getStyle(getElement(), "font-family"));
-        DOM.setStyleAttribute(estimatorElement, "fontStyle",
-                getStyle(getElement(), "font-style"));
-        DOM.setStyleAttribute(estimatorElement, "fontWeight",
-                getStyle(getElement(), "font-weight"));
-        DOM.setStyleAttribute(estimatorElement, "fontSize",
-                getStyle(getElement(), "font-size"));
-        // XXX this might be required?
-        // DOM.setStyleAttribute(span, "borderLeftWidth",
-        // getStyle(getElement(), "border-left-width"));
-        // DOM.setStyleAttribute(span, "borderRightWidth",
-        // getStyle(getElement(), "border-right-width"));
-        // DOM.setStyleAttribute(span, "paddingLeft",
-        // getStyle(getElement(), "padding-left"));
-        // DOM.setStyleAttribute(span, "paddingRight",
-        // getStyle(getElement(), "padding-right"));
-
-        estimatorElement.setInnerText(text);
-
-        RootPanel.get().getElement().appendChild(estimatorElement);
-        int width = estimatorElement.getOffsetWidth();
-        RootPanel.get().getElement().removeChild(estimatorElement);
+        int width = new TextBoundsEstimator().getTextWidth(text, getElement());
 
         // we add some extra width for the focus indicator etc
         width += EXTRA_WIDTH;
