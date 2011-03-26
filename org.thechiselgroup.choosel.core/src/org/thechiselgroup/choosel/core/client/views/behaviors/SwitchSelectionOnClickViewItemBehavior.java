93d36a8744b657ffbc10c1e336f8bceecf3ccac6
Lars.Grammel <Lars.Grammel@2c9d0ac1-b0dc-320a-7a4d-fdcaf38e295f>
2011-03-26 20:09:27 +0000

    reorganized & refactored view-related classes

---
+++ org.thechiselgroup.choosel.core/src/org/thechiselgroup/choosel/core/client/views/behaviors/SwitchSelectionOnClickViewItemBehavior.java
@@ -0,0 +1,56 @@
+/*******************************************************************************
+ * Copyright 2009, 2010 Lars Grammel 
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License. 
+ * You may obtain a copy of the License at 
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0 
+ *     
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an "AS IS" BASIS, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+ * See the License for the specific language governing permissions and 
+ * limitations under the License.  
+ *******************************************************************************/
+package org.thechiselgroup.choosel.core.client.views.behaviors;
+
+import org.thechiselgroup.choosel.core.client.views.model.SelectionModel;
+import org.thechiselgroup.choosel.core.client.views.model.ViewItem;
+import org.thechiselgroup.choosel.core.client.views.model.ViewItemBehavior;
+import org.thechiselgroup.choosel.core.client.views.model.ViewItemInteraction;
+
+/**
+ * Manages {@link ViewItem} highlighting in a single view.
+ */
+public class SwitchSelectionOnClickViewItemBehavior implements ViewItemBehavior {
+
+    private SelectionModel selectionModel;
+
+    public SwitchSelectionOnClickViewItemBehavior(SelectionModel selectionModel) {
+        assert selectionModel != null;
+
+        this.selectionModel = selectionModel;
+    }
+
+    @Override
+    public void onInteraction(ViewItem viewItem, ViewItemInteraction interaction) {
+        assert viewItem != null;
+        assert interaction != null;
+
+        switch (interaction.getEventType()) {
+        case CLICK:
+            selectionModel.switchSelection(viewItem.getResources());
+            break;
+        }
+    }
+
+    @Override
+    public void onViewItemCreated(ViewItem viewItem) {
+    }
+
+    @Override
+    public void onViewItemRemoved(ViewItem viewItem) {
+    }
+
+}
