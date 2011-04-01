e96d472d34f43426c479a1895d032a5993ae5e20
Lars.Grammel <Lars.Grammel@2c9d0ac1-b0dc-320a-7a4d-fdcaf38e295f>
2011-04-01 18:42:50 +0000

    implemented pan & zoom

--- trunk/org.thechiselgroup.choosel.protovis/examples/org/thechiselgroup/choosel/protovis/client/ScatterplotExample.java
+++ trunk/org.thechiselgroup.choosel.protovis/examples/org/thechiselgroup/choosel/protovis/client/ScatterplotExample.java
@@ -38,22 +38,6 @@ import com.google.gwt.user.client.ui.Widget;
 public class ScatterplotExample extends ProtovisWidget implements
         ProtovisExample {
 
-    public static class Triple {
-
-        public double x;
-
-        public double y;
-
-        public double z;
-
-        private Triple(double x, double y, double z) {
-            this.x = x;
-            this.y = y;
-            this.z = z;
-        }
-
-    }
-
     @Override
     public Widget asWidget() {
         return this;
