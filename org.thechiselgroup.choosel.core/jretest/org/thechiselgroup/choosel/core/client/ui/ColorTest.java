a6a7419455cfe0409face7ee8bac01afeb4bdc46
Lars.Grammel <Lars.Grammel@2c9d0ac1-b0dc-320a-7a4d-fdcaf38e295f>
2011-05-08 04:42:28 +0000

    refactored Color and ColorTest

--- org.thechiselgroup.choosel.core/jretest/org/thechiselgroup/choosel/core/client/ui/ColorTest.java
+++ org.thechiselgroup.choosel.core/jretest/org/thechiselgroup/choosel/core/client/ui/ColorTest.java
@@ -1,89 +1,82 @@
 package org.thechiselgroup.choosel.core.client.ui;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
 import org.junit.Test;
 
 public class ColorTest {
 
     @Test
-    public void blueGreenHex() {
-        testColor(0, 74, 192, 1.0, new Color("#004Ac0"));
-    }
-
-    @Test
-    public void blueHex() {
-        testColor(0, 0, 131, 1.0, new Color("#000083"));
-    }
-
-    @Test
-    public void fullblueHex() {
-        testColor(0, 0, 255, 1.0, new Color("#0000ff"));
-    }
-
-    @Test
-    public void fullgreenHex() {
-        testColor(0, 255, 0, 1.0, new Color("#00ff00"));
-    }
-
-    @Test
-    public void fullredblueGreen() {
-        testColor(255, 255, 255, 1.0, new Color("#ffffff"));
-    }
-
-    @Test
-    public void fullredHex() {
-        testColor(255, 0, 0, 1.0, new Color("#ff0000"));
-    }
-
-    @Test
-    public void greenHex() {
-        testColor(0, 207, 0, 1.0, new Color("#00Cf00"));
-    }
-
-    @Test
-    public void invalidInput() {
-        try {
-            Color color = new Color("00-000");
-            fail("Colour should have been invalid character");
-        } catch (IllegalArgumentException e) {
-
-        }
-    }
-
-    @Test
-    public void noColorHex() {
+    public void hex000000() {
         testColor(0, 0, 0, 1.0, new Color("#000000"));
     }
 
     @Test
-    public void outOfRange() {
-        try {
-            Color color = new Color("#00g000");
-            fail("Hex shoudl have been out of range");
-        } catch (IllegalArgumentException e) {
-
-        }
+    public void hex0000ff() {
+        testColor(0, 0, 255, 1.0, new Color("#0000ff"));
     }
 
     @Test
-    public void redBlueGreen() {
-        testColor(120, 154, 188, 1.0, new Color("#789abc"));
+    public void hex004ac0() {
+        testColor(0, 74, 192, 1.0, new Color("#004ac0"));
     }
 
     @Test
-    public void redBlueHex() {
+    public void hex00Cf00() {
+        testColor(0, 207, 0, 1.0, new Color("#00Cf00"));
+    }
+
+    @Test
+    public void hex2a0000() {
+        testColor(42, 0, 0, 1.0, new Color("#2a0000"));
+    }
+
+    @Test
+    public void hex3100d6() {
         testColor(49, 0, 214, 1.0, new Color("#3100d6"));
     }
 
     @Test
-    public void redGreenHex() {
+    public void hex3f6d00() {
         testColor(63, 109, 0, 1.0, new Color("#3f6d00"));
     }
 
     @Test
-    public void redHex() {
-        testColor(42, 0, 0, 1.0, new Color("#2a0000"));
+    public void hex789abc() {
+        testColor(120, 154, 188, 1.0, new Color("#789abc"));
+    }
+
+    @Test
+    public void hexff0000() {
+        testColor(255, 0, 0, 1.0, new Color("#ff0000"));
+    }
+
+    @Test
+    public void hext00ff00() {
+        testColor(0, 255, 0, 1.0, new Color("#00ff00"));
+    }
+
+    @Test
+    public void hextffffff() {
+        testColor(255, 255, 255, 1.0, new Color("#ffffff"));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void invalidInput() {
+        new Color("00-000");
+        fail("Should have thrown IllegalArgumentException: hex contains invalid character");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void outOfRange() {
+        new Color("#00g000");
+        fail("Should have thrown IllegalArgumentException: hex contains invalid character");
+    }
+
+    @Test
+    public void semiBlueHex() {
+        testColor(0, 0, 131, 1.0, new Color("#000083"));
     }
 
     private void testColor(int red, int green, int blue, double alpha,
