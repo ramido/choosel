caa6351f000a44923ebc3e117a27eb0f5427b87b
Lars.Grammel <Lars.Grammel@2c9d0ac1-b0dc-320a-7a4d-fdcaf38e295f>
2011-05-08 04:55:39 +0000

    refactored Color and ColorTest

--- org.thechiselgroup.choosel.core/jretest/org/thechiselgroup/choosel/core/client/ui/ColorTest.java
+++ org.thechiselgroup.choosel.core/jretest/org/thechiselgroup/choosel/core/client/ui/ColorTest.java
@@ -9,57 +9,62 @@ public class ColorTest {
 
     @Test
     public void hex000000() {
-        testColor(0, 0, 0, 1.0, new Color("#000000"));
+        assertEquals(new Color(0, 0, 0), new Color("#000000"));
+    }
+
+    @Test
+    public void hex000083() {
+        assertEquals(new Color(0, 0, 131), new Color("#000083"));
     }
 
     @Test
     public void hex0000ff() {
-        testColor(0, 0, 255, 1.0, new Color("#0000ff"));
+        assertEquals(new Color(0, 0, 255), new Color("#0000ff"));
     }
 
     @Test
     public void hex004ac0() {
-        testColor(0, 74, 192, 1.0, new Color("#004ac0"));
+        assertEquals(new Color(0, 74, 192), new Color("#004ac0"));
     }
 
     @Test
     public void hex00Cf00() {
-        testColor(0, 207, 0, 1.0, new Color("#00Cf00"));
+        assertEquals(new Color(0, 207, 0), new Color("#00Cf00"));
+    }
+
+    @Test
+    public void hex00ff00() {
+        assertEquals(new Color(0, 255, 0), new Color("#00ff00"));
     }
 
     @Test
     public void hex2a0000() {
-        testColor(42, 0, 0, 1.0, new Color("#2a0000"));
+        assertEquals(new Color(42, 0, 0), new Color("#2a0000"));
     }
 
     @Test
     public void hex3100d6() {
-        testColor(49, 0, 214, 1.0, new Color("#3100d6"));
+        assertEquals(new Color(49, 0, 214), new Color("#3100d6"));
     }
 
     @Test
     public void hex3f6d00() {
-        testColor(63, 109, 0, 1.0, new Color("#3f6d00"));
+        assertEquals(new Color(63, 109, 0), new Color("#3f6d00"));
     }
 
     @Test
     public void hex789abc() {
-        testColor(120, 154, 188, 1.0, new Color("#789abc"));
+        assertEquals(new Color(120, 154, 188), new Color("#789abc"));
     }
 
     @Test
     public void hexff0000() {
-        testColor(255, 0, 0, 1.0, new Color("#ff0000"));
+        assertEquals(new Color(255, 0, 0), new Color("#ff0000"));
     }
 
     @Test
-    public void hext00ff00() {
-        testColor(0, 255, 0, 1.0, new Color("#00ff00"));
-    }
-
-    @Test
-    public void hextffffff() {
-        testColor(255, 255, 255, 1.0, new Color("#ffffff"));
+    public void hexffffff() {
+        assertEquals(new Color(255, 255, 255), new Color("#ffffff"));
     }
 
     @Test(expected = IllegalArgumentException.class)
@@ -78,19 +83,6 @@ public class ColorTest {
     public void invalidHex00g000() {
         new Color("#00g000");
         fail("Should have thrown IllegalArgumentException: hex contains invalid character");
-    }
-
-    @Test
-    public void semiBlueHex() {
-        testColor(0, 0, 131, 1.0, new Color("#000083"));
-    }
-
-    private void testColor(int red, int green, int blue, double alpha,
-            Color underTest) {
-        assertEquals(red, underTest.getRed());
-        assertEquals(blue, underTest.getBlue());
-        assertEquals(green, underTest.getGreen());
-        assertEquals(alpha, underTest.getAlpha(), 0.0);
     }
 
 }
