6777d767f4f918c7cf7340e4febfc3bedbf47fa9
Lars.Grammel <Lars.Grammel@2c9d0ac1-b0dc-320a-7a4d-fdcaf38e295f>
2011-06-24 04:14:15 +0000

    branched issue 156

---
+++ branches/issue_156/org.thechiselgroup.choosel.workbench/src/org/thechiselgroup/choosel/workbench/client/ChooselWorkbenchViewWindowContentProducer.java
@@ -0,0 +1,76 @@
+/*******************************************************************************
+ * Copyright 2009, 2010 Lars Grammel 
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License. 
+ * You may obtain a copy of the License at 
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0 
+ *     
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an "AS IS" BASIS, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+ * See the License for the specific language governing permissions and 
+ * limitations under the License.  
+ *******************************************************************************/
+package org.thechiselgroup.choosel.workbench.client;
+
+import java.util.Date;
+
+import org.thechiselgroup.choosel.core.client.resources.DataType;
+import org.thechiselgroup.choosel.core.client.ui.Color;
+import org.thechiselgroup.choosel.core.client.util.collections.LightweightList;
+import org.thechiselgroup.choosel.core.client.views.ViewPart;
+import org.thechiselgroup.choosel.core.client.views.model.DefaultSlotMappingInitializer;
+import org.thechiselgroup.choosel.core.client.views.model.SlotMappingInitializer;
+import org.thechiselgroup.choosel.core.client.views.resolvers.FixedValueResolver;
+import org.thechiselgroup.choosel.workbench.client.ui.configuration.ViewWindowContentProducer;
+import org.thechiselgroup.choosel.workbench.client.workspace.ShareConfigurationFactory;
+import org.thechiselgroup.choosel.workbench.client.workspace.ShareConfigurationViewPart;
+
+import com.google.inject.Inject;
+
+public class ChooselWorkbenchViewWindowContentProducer extends
+        ViewWindowContentProducer {
+
+    @Inject
+    private ShareConfigurationFactory shareConfigurationFactory;
+
+    @Override
+    protected SlotMappingInitializer createSlotMappingInitializer(
+            String contentType) {
+
+        DefaultSlotMappingInitializer initializer = new DefaultSlotMappingInitializer();
+
+        initializer.putDefaultDataTypeValues(DataType.NUMBER,
+                new FixedValueResolver(new Double(0), "Fixed-0",
+                        DataType.NUMBER));
+        initializer
+                .putDefaultDataTypeValues(DataType.SHAPE,
+                        new FixedValueResolver("circle", "Fixed-circle",
+                                DataType.SHAPE)); // TODO
+        // PVShape.CIRCLE
+        initializer.putDefaultDataTypeValues(DataType.COLOR,
+                new FixedValueResolver(new Color(100, 149, 237),
+                        "fixed-stdblue", DataType.COLOR));
+        initializer
+                .putDefaultDataTypeValues(DataType.DATE,
+                        new FixedValueResolver(new Date(), "Fixed-date",
+                                DataType.DATE));
+        initializer.putDefaultDataTypeValues(DataType.TEXT,
+                new FixedValueResolver("", "Fixed-emptystr", DataType.TEXT));
+
+        return initializer;
+    }
+
+    @Override
+    protected LightweightList<ViewPart> createViewParts(String contentType) {
+        LightweightList<ViewPart> parts = super.createViewParts(contentType);
+
+        parts.add(new ShareConfigurationViewPart(shareConfigurationFactory
+                .createShareConfiguration()));
+
+        return parts;
+    }
+
+}
