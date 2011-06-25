7a70c9485bdf906a8577c945d469a639626903a6
Lars.Grammel <Lars.Grammel@2c9d0ac1-b0dc-320a-7a4d-fdcaf38e295f>
2011-06-25 20:53:43 +0000

    ACCEPTED - task 156: The Visual Mappings Control Do Not Reflect Default Slot Mappings http://code.google.com/p/choosel/issues/detail?id=156
    DefaultViewItemResolutionErrorModel

--- branches/issue_156/org.thechiselgroup.choosel.core/jretest/org/thechiselgroup/choosel/core/client/views/model/DefaultViewItemResolutionErrorModelTest.java
+++ branches/issue_156/org.thechiselgroup.choosel.core/jretest/org/thechiselgroup/choosel/core/client/views/model/DefaultViewItemResolutionErrorModelTest.java
@@ -31,69 +31,93 @@ public class DefaultViewItemResolutionErrorModelTest {
     private DefaultViewItemResolutionErrorModel underTest;
 
     @Mock
-    private ViewItem viewItem;
+    private ViewItem viewItem1;
 
-    private Slot slot;
+    @Mock
+    private ViewItem viewItem2;
+
+    private Slot slot1;
+
+    private Slot slot2;
 
     @Test
-    public void addErrorAddsSlotToErrorSlots() {
-        underTest.addError(slot, viewItem);
+    public void clearSlotDoesNotRemoveViewItemMarkIfOtherErrors() {
+        underTest.reportError(slot1, viewItem1);
+        underTest.reportError(slot2, viewItem1);
+        underTest.clearErrors(slot1);
 
-        assertThat(underTest.getSlotsWithErrors(), containsExactly(slot));
+        assertThat(underTest.hasErrors(viewItem1), is(true));
     }
 
     @Test
-    public void addErrorAddsSlotToViewItemErrors() {
-        underTest.addError(slot, viewItem);
+    public void clearSlotRemovesSlotFromViewItemErrors() {
+        underTest.reportError(slot1, viewItem1);
+        underTest.reportError(slot2, viewItem1);
+        underTest.clearErrors(slot1);
 
-        assertThat(underTest.getSlotsWithErrors(viewItem),
-                containsExactly(slot));
+        assertThat(underTest.getSlotsWithErrors(viewItem1),
+                containsExactly(slot2));
     }
 
     @Test
-    public void addErrorAddsViewItemToErrorViewItems() {
-        underTest.addError(slot, viewItem);
+    public void clearSlotRemovesSlotMark() {
+        underTest.reportError(slot1, viewItem1);
+        underTest.clearErrors(slot1);
 
-        assertThat(underTest.getViewItemsWithErrors(slot),
-                containsExactly(viewItem));
+        assertThat(underTest.hasErrors(slot1), is(false));
     }
 
     @Test
-    public void addErrorAddsViewItemToSlotErrors() {
-        underTest.addError(slot, viewItem);
+    public void clearSlotRemovesViewItemMarkIfNoOtherErrors() {
+        underTest.reportError(slot1, viewItem1);
+        underTest.clearErrors(slot1);
 
-        assertThat(underTest.getSlotsWithErrors(), containsExactly(slot));
+        assertThat(underTest.hasErrors(viewItem1), is(false));
     }
 
     @Test
-    public void addErrorMarksSlotAsErroneous() {
-        underTest.addError(slot, viewItem);
+    public void clearViewItemDoesNotRemovesSlotMarkIfOtherErrors() {
+        underTest.reportError(slot1, viewItem1);
+        underTest.reportError(slot1, viewItem2);
+        underTest.clearErrors(viewItem1);
 
-        assertThat(underTest.hasErrors(slot), is(true));
+        assertThat(underTest.hasErrors(slot1), is(true));
     }
 
     @Test
-    public void addErrorMarksViewItemAsErroneous() {
-        underTest.addError(slot, viewItem);
+    public void clearViewItemRemovesSlotMarkIfNoOtherErrors() {
+        underTest.reportError(slot1, viewItem1);
+        underTest.clearErrors(viewItem1);
 
-        assertThat(underTest.hasErrors(viewItem), is(true));
+        assertThat(underTest.hasErrors(slot1), is(false));
     }
 
     @Test
-    public void addErrorSetsHasErrors() {
-        underTest.addError(slot, viewItem);
+    public void clearViewItemRemovesViewItemFromSlotErrors() {
+        underTest.reportError(slot1, viewItem1);
+        underTest.reportError(slot1, viewItem2);
+        underTest.clearErrors(viewItem1);
 
-        assertThat(underTest.hasErrors(), is(true));
+        assertThat(underTest.getViewItemsWithErrors(slot1),
+                containsExactly(viewItem2));
+    }
+
+    @Test
+    public void clearViewItemRemovesViewItemMark() {
+        underTest.reportError(slot1, viewItem1);
+        underTest.clearErrors(viewItem1);
+
+        assertThat(underTest.hasErrors(viewItem1), is(false));
     }
 
     @Test
     public void getSlotsWithErrorsReturnEmptyListIfViewItemIsErrorFree() {
-        assertThat(underTest.getSlotsWithErrors(viewItem).isEmpty(), is(true));
+        assertThat(underTest.getSlotsWithErrors(viewItem1).isEmpty(), is(true));
     }
 
     @Test
     public void getViewItemsWithErrorsReturnEmptyListIfSlotIsErrorFree() {
-        assertThat(underTest.getViewItemsWithErrors(slot).isEmpty(), is(true));
+        assertThat(underTest.getViewItemsWithErrors(slot1).isEmpty(), is(true));
     }
 
     @Test
@@ -101,13 +125,172 @@ public class DefaultViewItemResolutionErrorModelTest {
         assertThat(underTest.hasErrors(), is(false));
     }
 
+    @Test
+    public void removeDoesNotRemoveSlotItemMarkIfOtherErrors() {
+        underTest.reportError(slot1, viewItem1);
+        underTest.reportError(slot1, viewItem2);
+        underTest.removeError(slot1, viewItem1);
+
+        assertThat(underTest.hasErrors(slot1), is(true));
+    }
+
+    @Test
+    public void removeDoesNotRemoveViewItemMarkIfOtherErrors() {
+        underTest.reportError(slot1, viewItem1);
+        underTest.reportError(slot2, viewItem1);
+        underTest.removeError(slot1, viewItem1);
+
+        assertThat(underTest.hasErrors(viewItem1), is(true));
+    }
+
+    @Test
+    public void removeRemovesSlotFromViewItemErrors() {
+        underTest.reportError(slot1, viewItem1);
+        underTest.reportError(slot2, viewItem1);
+        underTest.removeError(slot1, viewItem1);
+
+        assertThat(underTest.getSlotsWithErrors(viewItem1),
+                containsExactly(slot2));
+    }
+
+    @Test
+    public void removeRemovesSlotItemMarkIfNoOtherErrors() {
+        underTest.reportError(slot1, viewItem1);
+        underTest.removeError(slot1, viewItem1);
+
+        assertThat(underTest.hasErrors(slot1), is(false));
+    }
+
+    @Test
+    public void removeRemovesSlotMark() {
+        underTest.reportError(slot1, viewItem1);
+        underTest.removeError(slot1, viewItem1);
+
+        assertThat(underTest.hasErrors(slot1), is(false));
+    }
+
+    @Test
+    public void removeRemovesViewItemFromSlotErrors() {
+        underTest.reportError(slot1, viewItem1);
+        underTest.reportError(slot1, viewItem2);
+        underTest.removeError(slot1, viewItem1);
+
+        assertThat(underTest.getViewItemsWithErrors(slot1),
+                containsExactly(viewItem2));
+    }
+
+    @Test
+    public void removeRemovesViewItemMark() {
+        underTest.reportError(slot1, viewItem1);
+        underTest.removeError(slot1, viewItem1);
+
+        assertThat(underTest.hasErrors(viewItem1), is(false));
+    }
+
+    @Test
+    public void removeRemovesViewItemMarkIfNoOtherErrors() {
+        underTest.reportError(slot1, viewItem1);
+        underTest.removeError(slot1, viewItem1);
+
+        assertThat(underTest.hasErrors(viewItem1), is(false));
+    }
+
+    @Test
+    public void reportErrorAddsSlotToErrorSlots() {
+        underTest.reportError(slot1, viewItem1);
+
+        assertThat(underTest.getSlotsWithErrors(), containsExactly(slot1));
+    }
+
+    @Test
+    public void reportErrorAddsSlotToViewItemErrors() {
+        underTest.reportError(slot1, viewItem1);
+
+        assertThat(underTest.getSlotsWithErrors(viewItem1),
+                containsExactly(slot1));
+    }
+
+    @Test
+    public void reportErrorAddsViewItemToErrorViewItems() {
+        underTest.reportError(slot1, viewItem1);
+
+        assertThat(underTest.getViewItemsWithErrors(slot1),
+                containsExactly(viewItem1));
+    }
+
+    @Test
+    public void reportErrorAddsViewItemToViewItemErrors() {
+        underTest.reportError(slot1, viewItem1);
+
+        assertThat(underTest.getViewItemsWithErrors(),
+                containsExactly(viewItem1));
+    }
+
+    @Test
+    public void reportErrorMarksSlotAsErroneous() {
+        underTest.reportError(slot1, viewItem1);
+
+        assertThat(underTest.hasErrors(slot1), is(true));
+    }
+
+    @Test
+    public void reportErrorMarksViewItemAsErroneous() {
+        underTest.reportError(slot1, viewItem1);
+
+        assertThat(underTest.hasErrors(viewItem1), is(true));
+    }
+
+    @Test
+    public void reportErrorSetsHasErrors() {
+        underTest.reportError(slot1, viewItem1);
+
+        assertThat(underTest.hasErrors(), is(true));
+    }
+
+    @Test
+    public void reportErrorTwiceAddsSlotToErrorSlotsOnce() {
+        underTest.reportError(slot1, viewItem1);
+        underTest.reportError(slot1, viewItem1);
+
+        assertThat(underTest.getSlotsWithErrors(), containsExactly(slot1));
+    }
+
+    @Test
+    public void reportErrorTwiceAddsSlotToViewItemErrorsOnce() {
+        underTest.reportError(slot1, viewItem1);
+        underTest.reportError(slot1, viewItem1);
+
+        assertThat(underTest.getSlotsWithErrors(viewItem1),
+                containsExactly(slot1));
+    }
+
+    @Test
+    public void reportErrorTwiceAddsViewItemToErrorViewItemsOnce() {
+        underTest.reportError(slot1, viewItem1);
+        underTest.reportError(slot1, viewItem1);
+
+        assertThat(underTest.getViewItemsWithErrors(slot1),
+                containsExactly(viewItem1));
+    }
+
+    @Test
+    public void reportErrorTwiceAddsViewItemToViewItemErrorsOnce() {
+        underTest.reportError(slot1, viewItem1);
+        underTest.reportError(slot1, viewItem1);
+
+        assertThat(underTest.getViewItemsWithErrors(),
+                containsExactly(viewItem1));
+    }
+
     @Before
     public void setUp() {
         MockitoAnnotations.initMocks(this);
 
-        slot = new Slot("s1", "", DataType.TEXT);
+        slot1 = new Slot("s1", "", DataType.TEXT);
+        slot2 = new Slot("s2", "", DataType.TEXT);
 
-        when(viewItem.getViewItemID()).thenReturn("v1");
+        when(viewItem1.getViewItemID()).thenReturn("v1");
+        when(viewItem2.getViewItemID()).thenReturn("v2");
 
         underTest = new DefaultViewItemResolutionErrorModel();
     }
--- branches/issue_156/org.thechiselgroup.choosel.core/src/org/thechiselgroup/choosel/core/client/views/model/DefaultViewItemResolutionErrorModel.java
+++ branches/issue_156/org.thechiselgroup.choosel.core/src/org/thechiselgroup/choosel/core/client/views/model/DefaultViewItemResolutionErrorModel.java
@@ -16,12 +16,22 @@
 package org.thechiselgroup.choosel.core.client.views.model;
 
 import java.util.Map;
+import java.util.Map.Entry;
 
 import org.thechiselgroup.choosel.core.client.util.collections.ArrayListToLightweightListAdapter;
 import org.thechiselgroup.choosel.core.client.util.collections.CollectionFactory;
 import org.thechiselgroup.choosel.core.client.util.collections.LightweightCollection;
 import org.thechiselgroup.choosel.core.client.util.collections.LightweightCollections;
 
+/**
+ * <p>
+ * IMPLEMENTATION NOTE: This class assumes that the {@link Slot}s that are
+ * passed in have unique slot ids. If two different slots have the same slot id,
+ * the functionality in this class will not work.
+ * </p>
+ * 
+ * @author Lars Grammel
+ */
 public class DefaultViewItemResolutionErrorModel implements
         ViewItemResolutionErrorModel {
 
@@ -29,6 +39,8 @@ public class DefaultViewItemResolutionErrorModel implements
      * Contains ids of all slots that have errors as keys, and the
      * {@link ViewItem}s that could not be resolved for that slot as values.
      * Slots without errors must not be contained.
+     * 
+     * @see #assertInvariantIntegrity()
      */
     private Map<String, ArrayListToLightweightListAdapter<ViewItem>> errorsBySlotId = CollectionFactory
             .createStringMap();
@@ -37,6 +49,8 @@ public class DefaultViewItemResolutionErrorModel implements
      * Contains ids of all {@link ViewItem}s that have errors as keys, and the
      * {@link Slots}s that could not be resolved for that {@link ViewItem} as
      * values. ViewItems without errors must not be contained.
+     * 
+     * @see #assertInvariantIntegrity()
      */
     private Map<String, ArrayListToLightweightListAdapter<Slot>> errorsByViewItemId = CollectionFactory
             .createStringMap();
@@ -45,25 +59,20 @@ public class DefaultViewItemResolutionErrorModel implements
 
     private ArrayListToLightweightListAdapter<ViewItem> viewItemsWithErrors = new ArrayListToLightweightListAdapter<ViewItem>();
 
-    public void addError(Slot slot, ViewItem viewItem) {
-        assert slot != null;
-        assert viewItem != null;
-
-        addErrorViewItemToSlot(slot, viewItem);
-        addErrorSlotToViewItem(viewItem, slot);
-    }
-
     private void addErrorSlotToViewItem(ViewItem viewItem, Slot slot) {
         String viewItemId = viewItem.getViewItemID();
 
         if (!errorsByViewItemId.containsKey(viewItemId)) {
             errorsByViewItemId.put(viewItemId,
                     new ArrayListToLightweightListAdapter<Slot>());
+            viewItemsWithErrors.add(viewItem);
         }
 
-        // TODO test for non double add
-        errorsByViewItemId.get(viewItemId).add(slot);
-        viewItemsWithErrors.add(viewItem);
+        ArrayListToLightweightListAdapter<Slot> viewItemErrors = errorsByViewItemId
+                .get(viewItemId);
+        if (!viewItemErrors.contains(slot)) {
+            viewItemErrors.add(slot);
+        }
     }
 
     private void addErrorViewItemToSlot(Slot slot, ViewItem viewItem) {
@@ -72,17 +81,98 @@ public class DefaultViewItemResolutionErrorModel implements
         if (!errorsBySlotId.containsKey(slotId)) {
             errorsBySlotId.put(slotId,
                     new ArrayListToLightweightListAdapter<ViewItem>());
+            slotsWithErrors.add(slot);
         }
 
-        // TODO test for non double add
-        errorsBySlotId.get(slotId).add(viewItem);
-        slotsWithErrors.add(slot);
+        ArrayListToLightweightListAdapter<ViewItem> slotErrors = errorsBySlotId
+                .get(slotId);
+        if (!slotErrors.contains(viewItem)) {
+            slotErrors.add(viewItem);
+        }
+    }
+
+    private <T> void assertDoesNotContainEmptyLists(
+            Map<String, ArrayListToLightweightListAdapter<T>> map) {
+
+        for (Entry<String, ArrayListToLightweightListAdapter<T>> entry : map
+                .entrySet()) {
+            assert !entry.getValue().isEmpty() : "empty list at key "
+                    + entry.getKey();
+        }
+    }
+
+    private void assertErrorSlotsIntegrity() {
+        assert errorsBySlotId.size() == slotsWithErrors.size();
+        for (Slot slot : slotsWithErrors) {
+            assert errorsBySlotId.containsKey(slot.getId()) : "Slot with id "
+                    + slot.getId() + " not contained in slotsWithErrors";
+        }
+    }
+
+    /**
+     * There are the following class invariants:
+     * 
+     * <ul>
+     * <li>errorsBySlotId must not contain empty lists</li>
+     * <li>errorsByViewItemId must not contain empty lists</li>
+     * <li>errorsBySlotId keys must match Slots in slotsWithErrors</li>
+     * <li>errorsByViewItemId keys must match ViewItems in viewItemsWithErrors</li>
+     * </ul>
+     */
+    private void assertInvariantIntegrity() {
+        assertDoesNotContainEmptyLists(errorsBySlotId);
+        assertDoesNotContainEmptyLists(errorsByViewItemId);
+        assertErrorSlotsIntegrity();
+        assertViewItemsIntegrity();
+    }
+
+    private void assertViewItemsIntegrity() {
+        assert errorsByViewItemId.size() == viewItemsWithErrors.size();
+        for (ViewItem viewItem : viewItemsWithErrors) {
+            assert errorsByViewItemId.containsKey(viewItem.getViewItemID()) : "ViewItem with id "
+                    + viewItem.getViewItemID()
+                    + " not contained in viewItemsWithErrors";
+        }
     }
 
     public void clearErrors(Slot slot) {
+        assert slot != null;
+        assertInvariantIntegrity();
+
+        if (!slotsWithErrors.contains(slot)) {
+            return;
+        }
+
+        slotsWithErrors.remove(slot);
+        ArrayListToLightweightListAdapter<ViewItem> removedViewItems = errorsBySlotId
+                .remove(slot.getId());
+        for (ViewItem viewItem : removedViewItems) {
+            removeSlotFromViewItemErrors(slot, viewItem);
+        }
+
+        assertInvariantIntegrity();
+        assert !errorsBySlotId.containsKey(slot.getId());
+        assert !hasErrors(slot);
     }
 
     public void clearErrors(ViewItem viewItem) {
+        assert viewItem != null;
+        assertInvariantIntegrity();
+
+        if (!viewItemsWithErrors.contains(viewItem)) {
+            return;
+        }
+
+        viewItemsWithErrors.remove(viewItem);
+        ArrayListToLightweightListAdapter<Slot> removedSlots = errorsByViewItemId
+                .remove(viewItem.getViewItemID());
+        for (Slot slot : removedSlots) {
+            removeViewItemFromSlotErrors(viewItem, slot);
+        }
+
+        assertInvariantIntegrity();
+        assert !errorsByViewItemId.containsKey(viewItem.getViewItemID());
+        assert !hasErrors(viewItem);
     }
 
     @Override
@@ -137,7 +227,53 @@ public class DefaultViewItemResolutionErrorModel implements
     }
 
     public void removeError(Slot slot, ViewItem viewItem) {
+        assert viewItem != null;
+        assert slot != null;
+        assertInvariantIntegrity();
 
+        removeSlotFromViewItemErrors(slot, viewItem);
+        removeViewItemFromSlotErrors(viewItem, slot);
+
+        assertInvariantIntegrity();
+    }
+
+    public void removeSlotFromViewItemErrors(Slot slot, ViewItem viewItem) {
+        assert errorsByViewItemId.containsKey(viewItem.getViewItemID());
+
+        ArrayListToLightweightListAdapter<Slot> slotErrors = errorsByViewItemId
+                .get(viewItem.getViewItemID());
+        slotErrors.remove(slot);
+
+        // remove slot if no errors left
+        if (slotErrors.isEmpty()) {
+            errorsByViewItemId.remove(viewItem.getViewItemID());
+            viewItemsWithErrors.remove(viewItem);
+        }
+    }
+
+    private void removeViewItemFromSlotErrors(ViewItem viewItem, Slot slot) {
+        assert errorsBySlotId.containsKey(slot.getId());
+
+        ArrayListToLightweightListAdapter<ViewItem> slotErrors = errorsBySlotId
+                .get(slot.getId());
+        slotErrors.remove(viewItem);
+
+        // remove slot if no errors left
+        if (slotErrors.isEmpty()) {
+            errorsBySlotId.remove(slot.getId());
+            slotsWithErrors.remove(slot);
+        }
+    }
+
+    public void reportError(Slot slot, ViewItem viewItem) {
+        assert slot != null;
+        assert viewItem != null;
+        assertInvariantIntegrity();
+
+        addErrorViewItemToSlot(slot, viewItem);
+        addErrorSlotToViewItem(viewItem, slot);
+
+        assertInvariantIntegrity();
     }
 
 }
