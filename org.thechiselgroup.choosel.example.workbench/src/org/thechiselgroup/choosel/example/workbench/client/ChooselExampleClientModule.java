17dabe6e28dabe665666d6a9ad054108145838eb
Lars.Grammel <Lars.Grammel@2c9d0ac1-b0dc-320a-7a4d-fdcaf38e295f>
2011-07-11 06:44:53 +0000

    renaming

--- org.thechiselgroup.choosel.example.workbench/src/org/thechiselgroup/choosel/example/workbench/client/ChooselExampleClientModule.java
+++ org.thechiselgroup.choosel.example.workbench/src/org/thechiselgroup/choosel/example/workbench/client/ChooselExampleClientModule.java
@@ -19,7 +19,7 @@ import org.thechiselgroup.choosel.core.client.visualization.model.initialization
 import org.thechiselgroup.choosel.core.client.visualization.model.managed.VisualItemValueResolverFactoryProvider;
 import org.thechiselgroup.choosel.core.client.visualization.resolvers.managed.PreconfiguredVisualItemValueResolverFactoryProvider;
 import org.thechiselgroup.choosel.core.client.visualization.resolvers.ui.PreconfiguredViewItemResolverUIFactoryProvider;
-import org.thechiselgroup.choosel.core.client.visualization.resolvers.ui.ViewItemValueResolverUIControllerFactoryProvider;
+import org.thechiselgroup.choosel.core.client.visualization.resolvers.ui.VisualItemValueResolverUIControllerFactoryProvider;
 import org.thechiselgroup.choosel.visualization_component.graph.client.ArcTypeProvider;
 import org.thechiselgroup.choosel.visualization_component.graph.client.DefaultArcTypeProvider;
 import org.thechiselgroup.choosel.visualization_component.graph.client.GraphExpansionRegistry;
@@ -46,7 +46,7 @@ public class ChooselExampleClientModule extends ChooselWorkbenchClientModule {
     }
 
     @Override
-    protected Class<? extends ViewItemValueResolverUIControllerFactoryProvider> getResolverFactoryUIProviderClass() {
+    protected Class<? extends VisualItemValueResolverUIControllerFactoryProvider> getResolverFactoryUIProviderClass() {
         return PreconfiguredViewItemResolverUIFactoryProvider.class;
     }
 
