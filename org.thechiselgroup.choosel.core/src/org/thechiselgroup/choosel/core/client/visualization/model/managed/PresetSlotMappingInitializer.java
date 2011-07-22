6f467f32151244191dad4a27d2f412ed20584ca2
Lars.Grammel <Lars.Grammel@2c9d0ac1-b0dc-320a-7a4d-fdcaf38e295f>
2011-07-22 18:55:27 +0000

    extracted PresetSlotMappingInitializer fixed bio mixer slot intializatoin

---
+++ org.thechiselgroup.choosel.core/src/org/thechiselgroup/choosel/core/client/visualization/model/managed/PresetSlotMappingInitializer.java
@@ -0,0 +1,58 @@
+/*******************************************************************************
+ * Copyright (C) 2011 Lars Grammel 
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License. 
+ * You may obtain a copy of the License at 
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0 
+ *     
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an "AS IS" BASIS, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+ * See the License for the specific language governing permissions and 
+ * limitations under the License.  
+ *******************************************************************************/
+package org.thechiselgroup.choosel.core.client.visualization.model.managed;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.thechiselgroup.choosel.core.client.resources.ResourceSet;
+import org.thechiselgroup.choosel.core.client.visualization.model.Slot;
+import org.thechiselgroup.choosel.core.client.visualization.model.VisualItemValueResolver;
+import org.thechiselgroup.choosel.core.client.visualization.resolvers.managed.SingleSlotDependentVisualItemResolverFactory;
+
+public class PresetSlotMappingInitializer implements SlotMappingInitializer {
+
+    private final VisualItemValueResolverFactoryProvider provider;
+
+    private final Map<Slot, VisualItemValueResolver> slotMappings = new HashMap<Slot, VisualItemValueResolver>();
+
+    public PresetSlotMappingInitializer(
+            VisualItemValueResolverFactoryProvider provider) {
+
+        this.provider = provider;
+    }
+
+    @Override
+    public Map<Slot, VisualItemValueResolver> getResolvers(
+            ResourceSet viewResources, Map<Slot, ManagedSlotMappingState> states) {
+
+        return slotMappings;
+    }
+
+    @Override
+    public Map<Slot, VisualItemValueResolver> getResolvers(
+            ResourceSet viewResources, Slot[] slotsToUpdate) {
+
+        return slotMappings;
+    }
+
+    public void putSlotMapping(Slot slot, String resolverFactoryId) {
+
+        slotMappings.put(slot,
+                ((SingleSlotDependentVisualItemResolverFactory) provider
+                        .getFactoryById(resolverFactoryId)).create());
+    }
+}
